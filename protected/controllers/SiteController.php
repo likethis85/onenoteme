2d36f49ac5218ccceca6a9cde529052250d40401
Deng Chen <cdcchen@gmail.com>
2011-10-27 22:22:45 +0800

    update css

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -18,8 +18,8 @@ class SiteController extends Controller
     public function actionIndex()
     {
         $this->pageTitle = '挖段子';
-        $this->setKeywords('经典语录,糗事百科,秘密,笑话段子,笑话大全,搞笑大全,我们爱讲冷笑话,哈哈笑');
-        $this->setDescription('网罗互联网各种精品段子，各种糗事，各种笑话，各种秘密，各种经典语录，应有尽有。烦了、累了、无聊了，就来挖段子逛一逛。');
+//        $this->setKeywords('经典语录,糗事百科,秘密,笑话段子,笑话大全,搞笑大全,我们爱讲冷笑话,哈哈笑');
+//        $this->setDescription('网罗互联网各种精品段子，各种糗事，各种笑话，各种秘密，各种经典语录，应有尽有。烦了、累了、无聊了，就来挖段子逛一逛。');
         $this->forward('post/latest');
     }
     
