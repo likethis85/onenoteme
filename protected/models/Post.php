6c8e2ca3e08bea66253404df6fa0536e7c614051
Deng Chen <cdcchen@gmail.com>
2011-10-30 23:34:27 +0800

    xx

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -70,6 +70,7 @@ class Post extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+		    'user' => array(self::BELONGS_TO, 'User', 'user_id'),
 		);
 	}
 
