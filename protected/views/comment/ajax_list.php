1d13328d129e99ac60fa5792b17a654365cb1b86
Dong Chen <cdcchen@gmail.com>
2011-11-09 17:32:40 +0800

    fixed css bug in comment list

--- protected/views/comment/ajax_list.php
+++ protected/views/comment/ajax_list.php
@@ -1,7 +1,7 @@
 <?php foreach ($models as $model):?>
 <ul class="comment-item">
-	<li><img src="http://www.qiushibaike.com/system/avabak/150036/thumb/UC_Photo_1.jpg" /></li>
-	<li><?php echo $model->commentUserName;?></li>
+	<li class="user-small-thumbnail"><img src="http://www.qiushibaike.com/system/avabak/150036/thumb/UC_Photo_1.jpg" /></li>
+	<li class="user-name"><?php echo $model->commentUserName;?></li>
 	<li class="comment-content"><?php echo $model->content;?></li>
 	<div class="clear"></div>
 </ul>
--- protected/views/comment/list.php
+++ protected/views/comment/list.php
@@ -1,8 +1,8 @@
 <div class="comment-list">
     <?php foreach ($models as $c):?>
     <ul class="comment-item">
-    	<li><img src="http://www.qiushibaike.com/system/avatars/289248/thumb/20111009173804159.jpg" /></li>
-        <li><?php echo CHtml::link($c->commentUserName, '#', array('target'=>'_blank'));?></li>
+    	<li class="user-small-thumbnail"><img src="http://www.qiushibaike.com/system/avatars/289248/thumb/20111009173804159.jpg" /></li>
+        <li class="user-name"><?php echo CHtml::link($c->commentUserName, '#', array('target'=>'_blank'));?></li>
         <li class="comment-content"><?php echo $c->content;?></li>
         <div class="clear"></div>
     </ul>
