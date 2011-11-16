5dba01c87cd0dc3b51f0302e1c535740170c897a
Dong Chen <cdcchen@gmail.com>
2011-11-16 10:43:05 +0800

    add sns share button in post list

--- protected/views/post/list.php
+++ protected/views/post/list.php
@@ -3,13 +3,16 @@
     <div class="post-item">
     	<div class="post-user"><?php echo $model->PostUserName . '&nbsp;' . $model->createTime;?></div>
         <div class="item-detail">
-            <a href="<?php echo aurl('post/show', array('id'=>$model->id));?>" target="_blank" title="新窗口中查看段子">: :</a>
-            <?php echo $model->content;?>
+            <a class="item-link" href="<?php echo aurl('post/show', array('id'=>$model->id));?>" target="_blank" title="新窗口中查看段子">: :</a>
+            <div class="item-content"><?php echo $model->content;?></div>
         </div>
         <?php if ($model->tags):?><div class="post-tags"><span class="cgray">标签：</span><?php echo $model->tagsLinks;?></div><?php endif;?>
         <ul class="item-toolbar cgray" postid="<?php echo $model->id;?>">
         	<li class="upscore fl" pid="<?php echo $model->id;?>"><?php echo $model->up_score;?></li>
         	<li class="downscore fl" pid="<?php echo $model->id;?>"><?php echo $model->down_score;?></li>
+        	<li class="fr"><span class="sns-share qzone"></span></li>
+        	<li class="fr"><span class="sns-share qqt"></span></li>
+        	<li class="fr"><span class="sns-share weibo"></span></li>
         	<li class="comment-nums fr">
         	    <a href="javascript:void(0);" url="<?php echo aurl('comment/list', array('pid'=>$model->id));?>" title="查看评论" class="view-comments" pid="<?php echo $model->id;?>"><?php echo $model->comment_nums;?>条评论</a>
         	    <a href="<?php echo aurl('post/show', array('id'=>$model->id), '', 'comment-list');?>" title="新窗口中查看查看评论" target="_blank">: :</a>
@@ -33,5 +36,27 @@ $(function(){
 		function(e){$(this).addClass('post-item-hover');},
 		function(e){$(this).removeClass('post-item-hover');}
 	);
+
+	$('.post-item .weibo').click(function(e){
+		var url = $(this).parents('.post-item').find('.item-link').attr('href');
+		var content = encodeURIComponent('#挖段子冷笑话#') + $(this).parents('.post-item').find('.item-content').text();
+	    shareToWeibo(url, content);
+	});
+	
+	$('.post-item .qqt').click(function(e){
+		var url = $(this).parents('.post-item').find('.item-link').attr('href');
+		var content = encodeURIComponent('#挖段子冷笑话#') + $(this).parents('.post-item').find('.item-content').text();
+	    shareToQQT(url, content);
+	});
+	
+	$('.post-item .qzone').click(function(e){
+		var url = $(this).parents('.post-item').find('.item-link').attr('href');
+		var content = encodeURIComponent('#挖段子冷笑话#') + $(this).parents('.post-item').find('.item-content').text();
+	    shareToQzone(url, content);
+	});
 });
 </script>
+
+
+
+<?php cs()->registerScriptFile(sbu('scripts/snsshare.js'), CClientScript::POS_END);?>
--- resources/scripts/snsshare.js
+++ resources/scripts/snsshare.js
@@ -17,3 +17,45 @@ function postToWb(title)
     var _u = 'http://share.v.t.qq.com/index.php?c=share&a=index&url='+_url+'&appkey='+_appkey+'&pic='+_pic+'&assname='+_assname+'&title='+_t;
     window.open( _u,'', 'width=700, height=680, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, location=yes, resizable=no, status=no' );
 }
+
+function shareToWeibo(url, content)
+{
+	var _url = encodeURIComponent(url);
+	var _assname = encodeURI("cdcchen");
+    var _appkey = encodeURI("8fcd940fb4e54897ad5d07320468d8fd");
+    var _t = content;
+    if (_t.length > 180)
+        _t= _t.substr(0,177) + '...';
+    var _pic = encodeURI('');
+	var _u = 'http://share.v.t.qq.com/index.php?c=share&a=index&url='+_url+'&appkey='+_appkey+'&pic='+_pic+'&assname='+_assname+'&title='+_t;
+    window.open( _u,'', 'width=700, height=680, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, location=yes, resizable=no, status=no' );
+}
+
+function shareToQQT(url, content)
+{
+	var _url = encodeURIComponent(url);
+	var _ralateUid = encodeURI("1639121454");
+    var _appkey = encodeURI("3658366445");
+    var _t = content;
+    if (_t.length > 170)
+        _t= _t.substr(0,167) + '...';
+    var _pic = encodeURI('');
+    var _u = 'http://service.weibo.com/share/share.php?url='+_url+'&appkey='+_appkey+'&title='+_t+'&pic='+_pic+'&ralateUid=' + _ralateUid;
+    window.open( _u,'', 'width=700, height=680, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, location=yes, resizable=no, status=no' );
+}
+
+function shareToQzone(url, content)
+{
+	var _url = encodeURIComponent(url);
+	var _desc = encodeURIComponent('哈哈，一点也不好笑。');
+	var _ralateUid = encodeURI("1639121454");
+    var _appkey = encodeURI("3658366445");
+    var _site = encodeURIComponent('挖段子');
+    var _t = content;
+//    if (_t.length > 120)
+//        _t= _t.substr(0,117) + '...';
+    var _pic = encodeURI('');
+    var _u = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url='+_url+'&desc='+_desc+'&summary='+_t+'&title='+_desc+'&site='+_site+'&pics=' + _pic;
+    window.open( _u,'', 'width=700, height=680, top=0, left=0, toolbar=no, menubar=no, scrollbars=no, location=yes, resizable=no, status=no' );
+}
+
