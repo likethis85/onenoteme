1243bc6d771b0cbc96870216872ae43b11f5aef2
Dong Chen <cdcchen@gmail.com>
2012-01-07 00:07:22 +0800

    ee

--- protected/views/post/list.php
+++ protected/views/post/list.php
@@ -47,7 +47,7 @@ $(function(){
 
 	$('.item-pic').toggle(
 		function(e) {
-		    var imageHeight = $(this).height();
+		    var imageHeight = parseInt($(this).height()) + 30;
 		    $(this).parent().css('max-height', imageHeight);
 	    },
 	    function(e) {
