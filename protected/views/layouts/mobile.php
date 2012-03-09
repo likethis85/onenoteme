67e6117378291c643d80db28d32f6b7716bdbaa4
Dong Chen <cdcchen@gmail.com>
2012-03-10 00:15:04 +0800

    replace adsense code in mobile page

--- protected/views/layouts/mobile.php
+++ protected/views/layouts/mobile.php
@@ -33,14 +33,14 @@
   // XHTML should not attempt to parse these strings, declare them CDATA.
   /* <![CDATA[ */
   window.googleAfmcRequest = {
-    client: 'ca-mb-pub-9725980429199769',
+    client: 'ca-mb-pub-0852804202998726',
     format: '320x50_mb',
     output: 'HTML',
-    slotname: '1805318491',
+    slotname: '2500142833',
   };
   /* ]]> */
 //--></script>
-<script type="text/javascript"    src="http://pagead2.googlesyndication.com/pagead/show_afmc_ads.js"></script>
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_afmc_ads.js"></script>
 </div>
 <div class="m-wrapper"><?php echo $content;?></div>
 
