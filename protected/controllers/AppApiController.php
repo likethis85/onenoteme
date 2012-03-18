358b1a59948d9d793c725d00687a19aa67ea59b5
Dong Chen <cdcchen@gmail.com>
2012-03-18 15:19:43 +0800

    add AppApiController for iphone app 2.0 version

---
+++ protected/controllers/AppApiController.php
@@ -0,0 +1,127 @@
+<?php
+class AppApiController extends Controller
+{
+    public function actionNew($lastid, $cid = 0, $device_token = '')
+    {
+        if (empty($lastid))
+            self::output(array());
+        
+        $where = "t.state != :state and id > :lastid and pic = ''";
+        $params = array(':state' => DPost::STATE_DISABLED, ':lastid'=>$lastid);
+        if ($cid > 0) {
+            $where .= ' and category_id = :cid';
+            $params[':cid'] = $cid;
+        }
+        $cmd = app()->db->createCommand()
+            ->from('{{post}} t')
+            ->order('t.id desc')
+            ->where($where, $params);
+        
+        $rows = $cmd->queryAll();
+        
+        // 更新最后请求时间
+        self::updateLastRequestTime($device_token);
+        
+        self::output($rows);
+    }
+    
+    public function actionLatest($lastid, $channelid = 0, $device_token = '')
+    {
+        $lastid = (int)$lastid;
+        $channelid = (int)$channelid;
+    
+        if (empty($lastid))
+            self::output(array());
+    
+        $where = "t.state != :state and id > :lastid and channel_id = :channelid";
+        $params = array(':state' => DPost::STATE_DISABLED, ':lastid'=>$lastid, ':channelid'=>$channelid);
+
+        $cmd = app()->db->createCommand()
+            ->from('{{post}} t')
+            ->order('t.id desc')
+            ->where($where, $params);
+    
+        $rows = $cmd->queryAll();
+    
+        // 更新最后请求时间
+        self::updateLastRequestTime($device_token);
+    
+        self::output($rows);
+    }
+    
+    public function actionChannel($channelid, $offset, $limit)
+    {
+        $channelid = (int)$channelid;
+        $offset = (int)$offset;
+        $limit = (int)$limit;
+        $limit = $limit ? $limit : 10;
+        $offset = $offset ? $offset : 0;
+        
+        $where = "t.state != :state and channel_id = :channelid";
+        $params = array(':state' => DPost::STATE_DISABLED, ':channelid'=>$channelid);
+        
+        $cmd = app()->db->createCommand()
+            ->from('{{post}} t')
+            ->order('t.id desc')
+            ->limit($limit)
+            ->offset($offset)
+            ->where($where, $params);
+    
+        $rows = $cmd->queryAll();
+        self::output($rows);
+    }
+    
+    public function actionDeviceToken()
+    {
+        if (request()->getIsPostRequest() && isset($_POST)) {
+            $token = trim($_POST['device_token']);
+            $token = trim($token, '<>');
+            $token = str_replace(' ', '', $token);
+            if (empty($token))
+                $result = -1;
+            else {
+                $model = Device::model()->findByAttributes(array('device_token'=>$token));
+                if ($model === null) {
+                    $model = new Device();
+                    $model->device_token = $token;
+                    $model->last_time = $_SERVER['REQUEST_TIME'];
+                    $result = (int)$model->save();
+                }
+                else
+                    $result = 2;
+            }
+        }
+        else
+            $result = -2;
+        
+        self::output($result);
+    }
+    
+    private static function output($rows)
+    {
+        $format = strtolower(trim($_REQUEST['format']));
+        
+        if ($format === 'jsonp') {
+            header('Content-Type: application/javascript');
+            echo $_GET['callback'] . '(' . CJSON::encode($rows) . ')';
+        }
+        else {
+            header('Content-Type: application/json');
+            echo CJSON::encode($rows);
+        }
+        exit(0);
+    }
+
+    private static function updateLastRequestTime($deviceToken)
+    {
+        if (empty($deviceToken))
+            return false;
+        
+        $token = trim($deviceToken, '<>');
+        $token = str_replace(' ', '', $token);
+        Device::model()->updateAll(array('last_time'=>$_SERVER['REQUEST_TIME']), 'device_token = :token', array(':token'=>$token));
+    }
+}
+
+
+
