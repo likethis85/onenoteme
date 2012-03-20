95577aabe8d967e6ed6ae508f5a4e49105274d30
Dong Chen <cdcchen@gmail.com>
2012-03-20 21:27:04 +0800

    add mobile aboutme

---
+++ protected/views/mobile/aboutme.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=<?php echo app()->charset;?>" />
+<title><?php echo $title;?></title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+<meta content="yes" name="apple-mobile-web-app-capable" />
+<meta content="black" name="apple-mobile-web-app-status-bar-style" />
+<meta content="telephone=no" name="format-detection" />
+<style type="text/css">
+body {font-size:16px; line-height:24px; color:#333; background:#EFEFEF; margin:0; padding:0; width:100%; height:100%;}
+.phone-container {background:white; padding:7px;margin:7px; height:100%;}
+.post-title {text-align:center; font-size:20px; padding:10px 0 15px 0; margin:0; border-bottom:1px solid #DFDFDF; text-shadow:1px 1px #DFDFDF;}
+.phone-container img {max-width:300px;}
+</style>
+</head>
+<body>
+<div class="phone-container">
+    <h3 class="post-title">关于我们</h3>
+    <div class="content">
+        <ul>
+            <li><h3>当前最新版本：v2.0</h3></li>
+            <li>
+                <h3>应用及网站简介</h3>
+                <p>2.0版本经过重新设计，重写了全部代码，注重每日更新，对以往内容的浏览采用精品推荐的翻页方式，更加方便用户使用，同时添加了多种分享方式、图片可能保存到相册、段子可以进行复制、内容字体可以选择。</p>
+                <p>挖段子，致力于收集各种类别的中文笑话、经典语录。平时总会在网上浏览到各种各样的好玩的经典的段子，看过之后就忘记在哪里了，于是我便生出这么个念头来，做一个小站把他们汇集起来，无聊了，工作累了，拿出来看看，乐一下，希望有相同爱好需求的，可以跟我一起建设这个小站。</p>
+            </li>
+            <li>
+                <h3>团队</h3>
+                <ul>
+                    <li>目前站长：东子(Chris Chen)，负责技术开发。邮箱：cdcchen@163.com</li>
+        　　         <li>设计：Peter</li>
+        　　         <li>编辑主要以网友为主，我们是一个开放的平台，希望有兴趣的朋友可以一起加入。</li>
+                </ul>
+            </li>
+            <li>
+                <h3>寄语</h3>
+                <p>我们是一个非赢利网站，主要以兴趣爱好为动力，我们的口号：笑死人不尝命。</p>
+            </li>
+            <li>
+                <h3>大事记</h3>
+                <ul>
+                    <li>2012-04 挖段子iPhone v2.0 正式登录App Store。2.0经过重新设计，更加适合用户进行浏览，彻底解决崩溃的问题。</li>
+        　　         <li>2012-02 挖段子iPhone v1.6 正式登录App Store。</li>
+        　　         <li>2011-12 挖段子iPhone v1.5 正式登录App Store。</li>
+        　　         <li>2011-11 挖段子iPhone v1.0 正式登录App Store。</li>
+        　　         <li>2011-10 挖段子第一版正式上线。</li>
+        　　         <li>2009.12 注册挖段子域名，本来想做一个类似ms office onenote的人应用类网站，后来由于精力问题搁浅</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
+
