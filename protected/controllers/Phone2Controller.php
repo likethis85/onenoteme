84289a30ef6b10ac25421eceed2ab730c92b7ca1
Dong Chen <cdcchen@gmail.com>
2012-03-27 12:33:49 +0800

    ss

--- protected/controllers/Phone2Controller.php
+++ protected/controllers/Phone2Controller.php
@@ -209,8 +209,6 @@ class Phone2Controller extends Controller
                 $result = $comment->save();
                 
                 if ($result) {
-                    $counters = array('comment_nums'=>1);
-                    Post::model()->updateCounters($counters, 'id = :postid', array(':postid'=>$postid));
                     $data = array('errno'=>0);
                 }
                 else
