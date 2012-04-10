e42a4800ca65119c3829f9bfe76c6a4dbcd1ad39
Dong Chen <chendong@e23.cn>
2012-04-10 19:33:06 +0800

    qqqq

--- protected/apis/Api_Post.php
+++ protected/apis/Api_Post.php
@@ -96,6 +96,9 @@ class Api_Post extends ApiBase
     
     public function timeline()
     {
+        return self::fetchTestRows();
+        ////////////////////////////////////
+        
         self::requiredParams(array('channelid'));
         $params = $this->filterParams(array('channelid', 'count', 'fields', 'lastid'));
         
@@ -130,6 +133,12 @@ class Api_Post extends ApiBase
     
     public function history()
     {
+        return self::fetchTestRows();
+        ////////////////////////////////////
+        
+        
+        
+        
         self::requiredParams(array('channelid', 'beforetime'));
         $params = $this->filterParams(array('channelid', 'count', 'fields', 'beforetime'));
         
@@ -164,6 +173,11 @@ class Api_Post extends ApiBase
     
     public function latest()
     {
+        return self::fetchTestRows();
+        ////////////////////////////////////
+        
+        
+        
         self::requiredParams(array('channelid'));
         $params = $this->filterParams(array('channelid', 'count', 'fields', 'lasttime'));
         
@@ -198,6 +212,10 @@ class Api_Post extends ApiBase
     
     public function random()
     {
+        return self::fetchTestRows();
+        ////////////////////////////////////
+        
+        
         self::requiredParams(array('channelid'));
         $params = $this->filterParams(array('channelid', 'count', 'fields'));
         
@@ -359,6 +377,20 @@ class Api_Post extends ApiBase
     	}
     }
     
+    private static function fetchTestRows()
+    {
+        $ids = array(14079,14078,14077,14071,14061,14060,14053,14049,14046,14044,14043,14042,14041,14038,14036,14035,14034,14033,14032,14031,14030,14029,14027,14026,14025,14024,14023,14022,14021,14020,14019,14018,14017,14016,14015,14014,14013,14012,14011,13995,13994,13993,13992,13991,13990,13989,13988,13987,13986,13985,13984,13983,13982,13980,13979);
+        $cmd = app()->getDb()->createCommand()
+            ->from(TABLE_NAME_POST)
+            ->where(array('in', 'id', $ids));
+        $rows = $cmd->queryAll();
+        
+        foreach ($rows as $index => $row)
+            $rows[$index] = self::formatRow($row);
+        
+        shuffle($rows);
+        return $rows;
+    }
 }
 
 
