024be7d80f16148aa75c2c79aae3740ec1a86d32
Dong Chen <cdcchen@gmail.com>
2012-04-21 01:23:10 +0800

    dfdf

--- protected/views/layouts/mobile.php
+++ protected/views/layouts/mobile.php
@@ -32,12 +32,12 @@
     <a href="http://s.waduanzi.com/android/waduanzi.apk" target="_blank">Android版正式上线啦！！点击直接下载。</a>
 </div>
 <div class="admob">
-	<script type="text/javascript" src="http://union.163.com/gs2/union/adjs/6156606/0/1?w=640&h=60"></script>
+	<script type="text/javascript" src="http://union.163.com/gs2/union/adjs/6156606/0/1?w=300&h=40"></script>
 </div>
 <div class="m-wrapper"><?php echo $content;?></div>
 
 <div class="admob">
-	<script type="text/javascript" src="http://union.163.com/gs2/union/adjs/6156606/0/1?w=640&h=60"></script>
+	<script type="text/javascript" src="http://union.163.com/gs2/union/adjs/6156606/0/1?w=300&h=40"></script>
 </div>
 
 <div class="m-wrapper m-footer">
