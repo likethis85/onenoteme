c037726d78ef59b3dacf3fc04ce92cf3116c978e
Dong Chen <cdcchen@gmail.com>
2012-05-05 04:33:26 +0800

    dbfb

--- protected/modules/admin/views/post/weibo.php
+++ protected/modules/admin/views/post/weibo.php
@@ -2,6 +2,7 @@
     <thead>
         <tr>
             <th class="span7">内容</th>
+            <th class="span1">#</th>
             <th>#</th>
         </tr>
     </thead>
@@ -16,6 +17,8 @@
                 <p><a href="<?php echo aurl('admin/post/weiboVerify', array('id'=>$model->id, 'channel_id'=>CHANNEL_DUANZI));?>" class="btn btn-small btn-success row-verify">段子</a></p>
                 <p><a href="<?php echo aurl('admin/post/weiboVerify', array('id'=>$model->id, 'channel_id'=>CHANNEL_LENGTU));?>" class="btn btn-small btn-success row-verify">冷图</a></p>
                 <p><a href="<?php echo aurl('admin/post/weiboVerify', array('id'=>$model->id, 'channel_id'=>CHANNEL_GIRL));?>" class="btn btn-small btn-success row-verify">福利</a></p>
+            </td>
+            <td>
                 <p><a href="<?php echo aurl('admin/post/weiboDelete', array('id'=>$model->id));?>" class="btn btn-small btn-warning row-delete">删除</a></p>
             </td>
         </tr>
