eb3b04fb2bd25328524da5db77e03869de664c49
Dong Chen <cdcchen@gmail.com>
2012-05-05 04:12:08 +0800

    bbbb

--- protected/modules/admin/controllers/PostController.php
+++ protected/modules/admin/controllers/PostController.php
@@ -33,7 +33,7 @@ class PostController extends AdminController
                 $post->thumbnail = $temp->thumbnail_pic;
                 $post->pic = $temp->bmiddle_pic;
                 $post->big_pic = $temp->original_pic;
-                $result = $post->save();print_r($post->getErrors());
+                $result = $post->save();
                 if ($result)
                     $temp->delete();
                 $data = (int)$result;
