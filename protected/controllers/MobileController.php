0d3239e0737ab5a2b3db9a8ada0a793696be7931
Dong Chen <chendong@e23.cn>
2012-05-19 12:56:52 +0800

    dfdfd

--- protected/controllers/MobileController.php
+++ protected/controllers/MobileController.php
@@ -10,26 +10,25 @@ class MobileController extends Controller
     
     public function actionIndex()
     {
-        echo __FILE__;
-//         $limit = self::COUNT_OF_PAGE;
+        $limit = self::COUNT_OF_PAGE;
         
-//         $criteria = new CDbCriteria();
-//         $criteria->addColumnCondition(array('state' => POST_STATE_ENABLED));
-//         $criteria->order = 'create_time desc, id desc';
-//         $criteria->limit = $limit;
+        $criteria = new CDbCriteria();
+        $criteria->addColumnCondition(array('state' => POST_STATE_ENABLED));
+        $criteria->order = 'create_time desc, id desc';
+        $criteria->limit = $limit;
         
-//         $count = Post::model()->count($criteria);
-//         $pages = new CPagination($count);
-//         $pages->setPageSize($limit);
-//         $pages->applyLimit($criteria);
+        $count = Post::model()->count($criteria);
+        $pages = new CPagination($count);
+        $pages->setPageSize($limit);
+        $pages->applyLimit($criteria);
         
-//         $models = Post::model()->findAll($cmd);
+        $models = Post::model()->findAll($cmd);
         
-//         $this->pageTitle = '挖段子 - 笑死人不尝命';
-//         $this->setKeywords('笑话大全,黄段子,爆笑短信,最新段子,最全的段子,经典语录,糗事百科,秘密,笑话段子,经典笑话,笑话大全,搞笑大全,我们爱讲冷笑话,哈哈笑');
-//         $this->setDescription('最新发布的段子。网罗互联网各种精品段子，各种糗事，各种笑话，各种秘密，各种经典语录，应有尽有。烦了、累了、无聊了，就来挖段子逛一逛。');
+        $this->pageTitle = '挖段子 - 笑死人不尝命';
+        $this->setKeywords('笑话大全,黄段子,爆笑短信,最新段子,最全的段子,经典语录,糗事百科,秘密,笑话段子,经典笑话,笑话大全,搞笑大全,我们爱讲冷笑话,哈哈笑');
+        $this->setDescription('最新发布的段子。网罗互联网各种精品段子，各种糗事，各种笑话，各种秘密，各种经典语录，应有尽有。烦了、累了、无聊了，就来挖段子逛一逛。');
         
-//         $this->channel = 'latest';
+        $this->channel = 'latest';
 //         $this->render('index', array(
 //         	'models' => $models,
 //             'pages' => $pages,
