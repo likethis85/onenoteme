8b640b7455ad9522c95df149d35796b8d8c2031a
Dong Chen <chendong@e23.cn>
2012-05-19 11:41:29 +0800

    dfdff

--- protected/commands/PostCommand.php
+++ protected/commands/PostCommand.php
@@ -5,26 +5,26 @@ class PostCommand extends CConsoleCommand
     {
         $cmd = app()->getDb()->createCommand()
             ->select('id')
-            ->from('{{post}}')
+            ->from(TABLE_POST)
             ->order('create_time asc, id asc')
             ->limit($count);
         
         $conditions = array('and', 'channel_id = :channelID', 'state = :disable_state');
         
-        $params = array(':disable_state'=>Post::STATE_DISABLED, ':channelID'=>CHANNEL_DUANZI);
+        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_DUANZI);
         $duanziIDs = $cmd->where($conditions, $params)->queryColumn();
         
-        $params = array(':disable_state'=>Post::STATE_DISABLED, ':channelID'=>CHANNEL_LENGTU);
+        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_LENGTU);
         $lengtuIDs = $cmd->where($conditions, $params)->queryColumn();
         
-        $params = array(':disable_state'=>Post::STATE_DISABLED, ':channelID'=>CHANNEL_GIRL);
+        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_GIRL);
         $fuliIDs = $cmd->where($conditions, $params)->queryColumn();
         
         $ids = array_merge($duanziIDs, $lengtuIDs, $fuliIDs);
         
         $nums = app()->getDb()->createCommand()
             ->update('{{post}}',
-                array('state'=>Post::STATE_ENABLED, 'create_time'=>(int)$_SERVER['REQUEST_TIME']),
+                array('state'=>POST_STATE_ENABLED, 'create_time'=>(int)$_SERVER['REQUEST_TIME']),
                 array('in', 'id', $ids)
             );
         
