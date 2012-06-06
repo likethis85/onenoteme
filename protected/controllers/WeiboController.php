b6832d456b925f88a67710fb8d36b92a5243c93c
Dong Chen <cdcchen@gmail.com>
2012-06-06 12:52:34 +0800

    rgrgrg

--- protected/controllers/WeiboController.php
+++ protected/controllers/WeiboController.php
@@ -1,11 +1,13 @@
 <?php
 class WeiboController extends Controller
 {
+    const APP_KEY = '2981913360';
+    const APP_SECRETE = 'f06fd0b530f3d9daa56db67e5e8610e1';
+    
     public function actionAuthorize()
     {
-        $appKey = '2981913360';
         $callback = aurl('weibo/callback');
-        $url = sprintf('https://api.weibo.com/oauth2/authorize?client_id=%s&response_type=code&redirect_uri=%s', $appKey, $callback);
+        $url = sprintf('https://api.weibo.com/oauth2/authorize?client_id=%s&response_type=code&redirect_uri=%s', self::APP_KEY, $callback);
         $this->redirect($url);
         exit(0);
     }
@@ -13,6 +15,17 @@ class WeiboController extends Controller
     public function actionCallback($code)
     {
         $code = strip_tags(trim($code));
+        $url = sprintf('https://api.weibo.com/oauth2/access_token?client_id=%s&client_secret=%s&grant_type=authorization_code&redirect_uri=%s&code=%s', self::APP_KEY, self::APP_SECRETE, $redirectUrl, $code);
+        $curl = new CdCurl();
+        $curl->get($url);
+        if ($curl->errno() != 0)
+            throw new CHttpException(503, '获取token出错');
+        else
+            print_r($curl->rawdata());
+    }
+    
+    public function actionTest()
+    {
         print_r($_REQUEST);
     }
 }
