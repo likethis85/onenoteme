608d69b28cc814ac74683a009685012ca4d7663a
Dong Chen <cdcchen@gmail.com>
2012-06-06 22:04:00 +0800

    hghgh

---
+++ protected/modules/admin/controllers/WeiboController.php
@@ -0,0 +1,93 @@
+<?php
+class WeiboController extends AdminController
+{
+    public function actionPosts()
+    {
+        $pageSize = 20;
+        $criteria = new CDbCriteria();
+        $criteria->addColumnCondition(array('weibo_id' => ''));
+        $criteria->limit = $pageSize;
+        $criteria->order = 't.id asc';
+        
+        $models = AdminPost::model()->findAll($criteria);
+        $data = array(
+            'models' => $models,
+        );
+        $this->render('posts', $data);
+    }
+    
+    public function actionCreate($id)
+    {
+        $id = (int)$id;
+        $model = AdminPost::model()->findByPk($id);
+        if ($model === null) {
+            echo 0;
+            exit(0);
+        }
+        
+        $picUrl = $model->getBmiddlePic();
+        if (empty($picUrl)) {
+            $result = self::update($model);
+        }
+        else {
+            $result = self::upload($model);
+        }
+        
+        echo ($result === false) ? 0 : 1;
+        exit(0);
+    }
+    
+    private static function update(AdminPost $model)
+    {
+        if (empty($model->content)) return false;
+        
+        $url = 'https://upload.api.weibo.com/2/statuses/update.json';
+        $data = array(
+            'source' => WEIBO_APP_KEY,
+            'access_token' => app()->session['access_token'],
+            'status' => $model->content,
+        );
+        
+        $curl = new CdCurl();
+        $curl->post($url, $data);
+//         $jsonText = $curl->rawdata();
+//         echo $jsonText;exit;
+        if ($curl->errno() == 0) {
+            $result = json_decode($curl->rawdata(), true);
+            return $result['id'];
+        }
+        else
+            return false;
+    }
+    
+    private static function upload(AdminPost $model)
+    {
+        if (empty($model->content)) return false;
+        
+        $curl = new CdCurl();
+        $curl->get($model->getBmiddlePic());
+        if ($curl->errno() == 0)
+            $picData = $curl->rawdata();
+        else
+            return false;
+        
+        $url = 'https://upload.api.weibo.com/2/statuses/upload.json';
+        $data = array(
+            'source' => WEIBO_APP_KEY,
+            'access_token' => app()->session['access_token'],
+            'status' => $model->content,
+            'pic' => $picData,
+        );
+        
+        $curl = new CdCurl();
+        $curl->post($url, $data);
+        $jsonText = $curl->rawdata();
+        echo $jsonText;exit;
+        if ($curl->errno() == 0) {
+            $result = json_decode($curl->rawdata(), true);
+            return $result['id'];
+        }
+        else
+            return false;
+    }
+}
---
+++ protected/modules/admin/views/weibo/posts.php
@@ -0,0 +1,49 @@
+<table class="table table-striped table-bordered post-list-table">
+    <thead>
+        <tr>
+            <th class="span7">内容</th>
+            <th>#</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ((array)$models as $model):?>
+        <tr>
+            <td class="weibo-text">
+                <p><?php echo $model->content;?></p>
+                <?php echo l(image($model->thumbnail_pic, ''), $model->bmiddle_pic, array('target'=>'_blank'));?>
+            </td>
+            <td>
+                <p><a href="<?php echo aurl('admin/weibo/create', array('id'=>$model->id));?>" class="btn btn-small btn-warning row-create">发布</a></p>
+            </td>
+        </tr>
+        <?php endforeach;?>
+    </tbody>
+</table>
+<a class="btn btn-small btn-success" href=''>重新载入</a>
+
+<script type="text/javascript">
+$(function(){
+	$(document).on('click', '.row-create', function(event){
+		event.preventDefault();
+		
+		var tthis = $(this);
+		var jqXhrOptions = {
+			url: tthis.attr('href'),
+			type: 'post',
+			dataType: 'text',
+			cache: false
+		};
+		var jqXhr = $.ajax(jqXhrOptions);
+		jqXhr.done(function(data){
+			if (data > 0)
+				tthis.parents('tr').remove();
+			else
+				alert('操作失败，请重试一下');
+		});
+		
+		jqXhr.fail(function(){
+			alert('请求失败，请重试一下');
+		});
+	});
+});
+</script>
