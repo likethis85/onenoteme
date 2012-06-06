0c8629c0ab2b9d3e4cd0ee8b0d4416f4ee9a2770
Dong Chen <cdcchen@gmail.com>
2012-06-06 23:34:18 +0800

    gegeg

--- protected/modules/admin/controllers/WeiboController.php
+++ protected/modules/admin/controllers/WeiboController.php
@@ -88,7 +88,7 @@ class WeiboController extends AdminController
     }
     
     public function actionTest()
-    {exit;
+    {
         $url = 'https://upload.api.weibo.com/2/statuses/upload.json';
 //         $url = 'https://api.weibo.com/2/statuses/update.json';
     
@@ -106,11 +106,12 @@ class WeiboController extends AdminController
             'source' => WEIBO_APP_KEY,
             'access_token' => app()->session['access_token'],
             'status' => '这是一条测试数据',
-            'pic' => $picfile,
+            'pic' => '@' . $picfile,
         );
         
         $curl = new CdCurl();
         $curl->post($url, $data);
+        unlink($picfile);
         if ($curl->errno() == 0) {
             $result = json_decode($curl->rawdata(), true);
             print_r($result);
