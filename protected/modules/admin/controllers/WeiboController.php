0f7c9ceee7b9916eca441cc3355567c0113f43c6
Dong Chen <cdcchen@gmail.com>
2012-06-13 15:48:31 +0800

    232323

--- protected/modules/admin/controllers/WeiboController.php
+++ protected/modules/admin/controllers/WeiboController.php
@@ -1,6 +1,88 @@
 <?php
 class WeiboController extends AdminController
 {
+    public function actionSinat()
+    {
+        $callback = aurl('weibo/sinacb');
+        $url = sprintf('https://api.weibo.com/oauth2/authorize?client_id=%s&response_type=code&redirect_uri=%s', WEIBO_APP_KEY, $callback);
+        $this->redirect($url);
+        exit(0);
+    }
+    
+    public function actionSinacb($code)
+    {
+        $code = strip_tags(trim($code));
+        $callback = aurl('weibo/sinacb');
+        $url = sprintf('https://api.weibo.com/oauth2/access_token?grant_type=authorization_code&redirect_uri=%s&code=%s', $callback, $code);
+        $curl = new CdCurl();
+        $curl->basic_auth(WEIBO_APP_KEY, WEIBO_APP_SECRET);
+        $curl->post($url);
+        if ($curl->errno() != 0)
+            throw new CHttpException(503, '获取access_token出错');
+        else {
+            $data = json_decode($curl->rawdata(), true);
+            if (empty($data))
+                throw new CException('获取access_token错误');
+        
+            $cacheTokenKey = 'sina_weibo_access_token';
+            $result1 = app()->cache->set($cacheTokenKey, $access_token, $expires_in);
+            $cacheUserIDKey = 'sina_weibo_user_id';
+            $result2 = app()->cache->set($cacheUserIDKey, $access_token, $expires_in);
+            echo $result1 && $result2 ? '授权登录成功' : '授权登录失败';
+        }
+    }
+    
+    public function actionQqt()
+    {
+        $callback = aurl('weibo/qqcb');
+        $url = sprintf('https://open.t.qq.com/cgi-bin/oauth2/authorize?client_id=%s&response_type=code&redirect_uri=%s', QQT_APP_KEY, $callback);
+        $this->redirect($url);
+        exit(0);
+    }
+    
+    public function actionQqtcb($code, $openid)
+    {
+        $code = strip_tags(trim($code));
+        $callback = aurl('weibo/qqcb');
+        $url = sprintf('https://open.t.qq.com/cgi-bin/oauth2/access_token?client_id=%s&client_secret=%s&grant_type=authorization_code&redirect_uri=%s&code=%s', QQT_APP_KEY, QQT_APP_SECRET, $callback, $code);
+        $curl = new CdCurl();
+        $curl->post($url);
+        if ($curl->errno() != 0)
+            throw new CHttpException(503, '获取token出错');
+        else {
+            $returnString = $curl->rawdata();
+            if (empty($returnString))
+                throw new CException('获取access_token错误');
+        
+            /*
+             * $access_token
+             * $expires_in
+             * $refresh_token
+            */
+            parse_str($returnString);
+        
+            $cacheTokenKey = 'qq_weibo_access_token';
+            $result1 = app()->cache->set($cacheTokenKey, $access_token, $expires_in);
+            $cacheUserIDKey = 'qq_weibo_user_id';
+            $result2 = app()->cache->set($cacheUserIDKey, $openid, $expires_in);
+            echo $result1 && $result2 ? '授权登录成功' : '授权登录失败';
+        }
+    }
+    
+    public function actionNetease()
+    {
+        
+    }
+    
+    public function actionTest()
+    {
+        echo app()->cache->get('qq_weibo_user_id') . '<br />';
+        echo app()->cache->get('qq_weibo_access_token') . '<br />';
+        echo app()->cache->get('sina_weibo_user_id') . '<br />';
+        echo app()->cache->get('sina_weibo_access_token') . '<br />';
+    }
+    
+    
     public function actionPosts()
     {
         $pageSize = 1;
