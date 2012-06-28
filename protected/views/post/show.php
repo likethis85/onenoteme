8de594050b02f95a31c663438f767e7dc2730d4e
Dong Chen <cdcchen@gmail.com>
2012-06-28 15:08:56 +0800

    65j56j

--- protected/views/post/show.php
+++ protected/views/post/show.php
@@ -85,6 +85,16 @@
         <?php endforeach;?>
         <div class="clear"></div>
     </div>
+    <?php if ($post->bmiddlePic):?>
+    <div class="cdc-block">
+        <script type="text/javascript">
+        alimama_pid="mm_12551250_2904829_10392377";
+        alimama_width=300;
+        alimama_height=250;
+        </script>
+        <script src="http://a.alimama.cn/inf.js" type="text/javascript"></script>
+    </div>
+    <?php endif;?>
 	<div class="panel panel15"><?php $this->widget('CDHotTags', array('title'=>'热门标签'));?></div>
 </div>
 <div class="clear"></div>
