e0ae5ee791a326a7dc526e3d6501a931d8743213
Dong Chen <cdcchen@gmail.com>
2012-09-27 18:13:47 +0800

    添加广告

--- protected/views/post/show.php
+++ protected/views/post/show.php
@@ -10,11 +10,11 @@
         <div class="content-block post-picture"><?php echo l(CHtml::image($post->bmiddlePic, $post->filterContent . ', ' . $post->getTagText(',')), aurl('post/originalpic', array('id'=>$post->id)), array('target'=>'_blank', 'title'=>$post->filterContent));?></div>
         <?php endif;?>
         <?php if ($post->channel_id == CHANNEL_DUANZI):?>
-        <div class="cdc-block">
-            <p><a href="http://s.click.taobao.com/t_8?e=7HZ6jHSTbIg8q6nQGu%2B62FZKBlLXgG2mwcp6cvy9HYCgmQ%3D%3D&p=mm_12551250_0_0">公猴英伦休闲女鞋休闲鞋女单鞋小白鞋小白皮鞋平底时尚单鞋女051</a></p>
-            <p><a href="http://s.click.taobao.com/t_8?e=7HZ6jHSTbIlJ0FqabRsutUHqDnE4%2B79bP24MnZw9DcCG7w%3D%3D&p=mm_12551250_0_0">【倔强的偏执】中长袖连衣裙秋装新款 秋款秋连衣裙OL1518</a></p>
-            <p><a href="http://s.click.taobao.com/t_8?e=7HZ6jHSTbIg8oI%2FbUXjChSS8HRuUVdw7bnV4OoAz1vtquA%3D%3D&p=mm_12551250_0_0">淘金币 2012春秋装新款连衣裙韩版V领毛衣裙子 时尚女装修身连衣</a></p>
-        </div>
+        <ul class="cdc-block">
+            <li><a target="_blank" href="http://s.click.taobao.com/t_8?e=7HZ6jHSTbIg8q6nQGu%2B62FZKBlLXgG2mwcp6cvy9HYCgmQ%3D%3D&p=mm_12551250_0_0">公猴英伦休闲女鞋休闲鞋女单鞋小白鞋小白皮鞋平底时尚单鞋女051</a></li>
+            <li><a target="_blank" href="http://s.click.taobao.com/t_8?e=7HZ6jHSTbIlJ0FqabRsutUHqDnE4%2B79bP24MnZw9DcCG7w%3D%3D&p=mm_12551250_0_0">【倔强的偏执】中长袖连衣裙秋装新款 秋款秋连衣裙OL1518</a></li>
+            <li><a target="_blank" href="http://s.click.taobao.com/t_8?e=7HZ6jHSTbIg8oI%2FbUXjChSS8HRuUVdw7bnV4OoAz1vtquA%3D%3D&p=mm_12551250_0_0">淘金币 2012春秋装新款连衣裙韩版V领毛衣裙子 时尚女装修身连衣</a></li>
+        </ul>
         <?php endif;?>
         <div class="toolbar radius3px">
     		<div class="content-block post-arrows fleft">
