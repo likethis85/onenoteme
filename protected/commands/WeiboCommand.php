fca9999499b100fa54d9a5cd34737034a1e31a2e
Dong Chen <cdcchen@gmail.com>
2012-10-18 22:50:22 +0800

    dfddf

--- protected/commands/WeiboCommand.php
+++ protected/commands/WeiboCommand.php
@@ -319,7 +319,6 @@ class WeiboCommand extends CConsoleCommand
     
         $postUrl = 'http://www.waduanzi.com/post-' . $model->id;
         $sinatShortUrl = self::sinatShortUrl($postUrl);
-        $urlLen = empty($sinatShortUrl) ? 0 : strlen($sinatShortUrl);
         $tail = '...' . $sinatShortUrl . ' @挖段子网';
         if ($model->tags)
             $tags = '#' . $model->getTagText('##') . '#';
@@ -365,8 +364,11 @@ class WeiboCommand extends CConsoleCommand
     
         $postUrl = 'http://www.waduanzi.com/post-' . $model->id;
         $sinatShortUrl = self::sinatShortUrl($postUrl);
-        $urlLen = empty($sinatShortUrl) ? 0 : strlen($sinatShortUrl);
-        $content = mb_substr($model->content, 0, 130 - $urlLen, app()->charset) . '...' . $sinatShortUrl . ' @挖段子网';
+        $tail = '...' . $sinatShortUrl . ' @挖段子网';
+        if ($model->tags)
+            $tags = '#' . $model->getTagText('##') . '#';
+        $subLen = 140 - mb_strlen($tags, app()->charset) - mb_strlen($tail, app()->charset);
+        $content = $tags . mb_substr($model->content, 0, $subLen, app()->charset) . $tail;
         $data = array(
             'source' => WEIBO_APP_KEY,
             'access_token' => app()->cache->get('sina_weibo_access_token'),
@@ -412,8 +414,11 @@ class WeiboCommand extends CConsoleCommand
     
         $postUrl = 'http://www.waduanzi.com/post-' . $model->id;
         $sinatShortUrl = self::sinatShortUrl($postUrl);
-        $urlLen = empty($sinatShortUrl) ? 0 : strlen($sinatShortUrl);
-        $content = mb_substr($model->content, 0, 130 - $urlLen, app()->charset) . '...' . $sinatShortUrl . ' @cdcchen';
+        $tail = '...' . $sinatShortUrl . ' @cdcchen';
+        if ($model->tags)
+            $tags = '#' . $model->getTagText('##') . '#';
+        $subLen = 140 - mb_strlen($tags, app()->charset) - mb_strlen($tail, app()->charset);
+        $content = $tags . mb_substr($model->content, 0, $subLen, app()->charset) . $tail;
         $data = array(
             'oauth_consumer_key' => QQT_APP_KEY,
             'access_token' => app()->cache->get('qq_weibo_access_token'),
@@ -449,8 +454,11 @@ class WeiboCommand extends CConsoleCommand
     
         $postUrl = 'http://www.waduanzi.com/post-' . $model->id;
         $sinatShortUrl = self::sinatShortUrl($postUrl);
-        $urlLen = empty($sinatShortUrl) ? 0 : strlen($sinatShortUrl);
-        $content = mb_substr($model->content, 0, 130 - $urlLen, app()->charset) . '...' . $sinatShortUrl . ' @cdcchen';
+        $tail = '...' . $sinatShortUrl . ' @cdcchen';
+        if ($model->tags)
+            $tags = '#' . $model->getTagText('##') . '#';
+        $subLen = 140 - mb_strlen($tags, app()->charset) - mb_strlen($tail, app()->charset);
+        $content = $tags . mb_substr($model->content, 0, $subLen, app()->charset) . $tail;
         $data = array(
             'oauth_consumer_key' => QQT_APP_KEY,
             'access_token' => app()->cache->get('qq_weibo_access_token'),
