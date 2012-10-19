fad0705ecc5bdfb2c5d4eb1fb029c52e3d5dd6b8
Dong Chen <cdcchen@gmail.com>
2012-10-19 15:24:19 +0800

    efefef

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -48,10 +48,10 @@ class Controller extends CController
             $html = sprintf('<li><a href="%s">注册</a></li>', aurl('site/signup'));
 			$html .= sprintf('<li class="user-login"><a class="fleft" href="%s">登录</a></li>', aurl('site/login'));
 			$html .= sprintf('<li class="sns-icon"><a class="fright" href="%s"><img src="%s" alt="用新浪微博账号登录s" /></a>', aurl('weibo/sinat'), sbu('images/weibo24.png'));
-			$html .= sprintf('<a class="fright" href="%s"><img src="" alt="" /></a></li>', aurl('weibo/qqt'), sbu('images/qqt24.png'));
+			$html .= sprintf('<a class="fright" href="%s"><img src="%s" alt="" /></a></li>', aurl('weibo/qqt'), sbu('images/qqt24.png'));
         }
         elseif (app()->session['image_url']) {
-            $html = sprintf('<li class="user-name"><a href="%s"><img src="%s" alt="" align="top" />%s</a></li>', app()->session['image_url'], user()->name);
+            $html = sprintf('<li class="user-name"><a href="%s"><img src="%s" alt="进入用户中心" align="top" />%s</a></li>', aurl('member/defaut/index'), app()->session['image_url'], user()->name);
 			$html .= sprintf('<li><a href="%s">退出</a></li>', aurl('site/logout'));
         }
         else {
