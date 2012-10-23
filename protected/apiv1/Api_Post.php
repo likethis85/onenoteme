24ac761a5021b4ea66cbefeb44e35b1f62466ce3
Dong Chen <cdcchen@gmail.com>
2012-10-23 14:41:41 +0800

    efefef

--- protected/apiv1/Api_Post.php
+++ protected/apiv1/Api_Post.php
@@ -331,7 +331,7 @@ class Api_Post extends ApiBase
         	        $im->resizeToHeight($thumbHeight);
         	    else
         	        $im->resizeToWidth($thumbWidth);
-        	    $im->crop($thumbWidth, $thumbHeight, true)
+        	    $im->crop($thumbWidth, $thumbHeight, ($post->channel_id == CHANNEL_GIRL))
         	        ->saveAsJpeg($thumbnailFileName);
         	    $post->thumbnail_width = $im->width();
         	    $post->thumbnail_height = $im->height();
