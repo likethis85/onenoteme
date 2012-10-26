7a1b549ace8135fb16ca0b357188bf09b63ebd67
Dong Chen <cdcchen@gmail.com>
2012-10-26 22:11:15 +0800

    wefwefwef

--- protected/modules/mobile/views/system/error.php
+++ protected/modules/mobile/views/system/error.php
@@ -1,4 +1,6 @@
 <div class="alert alert-block alert-error">
     <h4 class="alert-heading">Error:&nbsp;<?php echo $code;?></h4>
+    <p>File:&nbsp;<?php echo $file;?>
+    <p>Line:&nbsp;<?php echo $line;?>
     <p><?php echo $message;?></p>
 </div>
