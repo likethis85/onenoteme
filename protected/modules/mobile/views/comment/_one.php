59abe8c3a71c933da1fc13e6a7b5c7be9ff729a6
Dong Chen <cdcchen@gmail.com>
2012-10-26 23:20:45 +0800

    3434343

--- protected/modules/mobile/views/comment/_one.php
+++ protected/modules/mobile/views/comment/_one.php
@@ -4,8 +4,8 @@
     <dd class="beta-comment-content"><?php echo $comment->filterContent;?></dd>
     <dd class="beta-comment-toolbar">
         <a class="beta-comment-reply" href="javascript:void(0);" data-url="<?php echo $comment->replyUrl;?>" rel="nofollow">回复</a>
-        <a class="beta-comment-rating" href="javascript:void(0);" data-url="<?php echo $comment->supportUrl;?>" rel="nofollow">支持(<span class="beta-comment-join-nums"><?php echo $comment->up_nums;?></span>)</a>
-        <a class="beta-comment-rating" href="javascript:void(0);" data-url="<?php echo $comment->againstUrl;?>" rel="nofollow">反对(<span class="beta-comment-join-nums"><?php echo $comment->down_nums;?></span></a>
+        <a class="beta-comment-rating" href="javascript:void(0);" data-url="<?php echo $comment->supportUrl;?>" rel="nofollow">支持(<span class="beta-comment-join-nums"><?php echo $comment->up_score;?></span>)</a>
+        <a class="beta-comment-rating" href="javascript:void(0);" data-url="<?php echo $comment->againstUrl;?>" rel="nofollow">反对(<span class="beta-comment-join-nums"><?php echo $comment->down_score;?></span></a>
     </dd>
 </dl>
 <?php endforeach;?>
