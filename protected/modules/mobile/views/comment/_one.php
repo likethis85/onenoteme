5ef02bdbfa3c3ebc4d4b7e1562b579c945bbd90b
Dong Chen <cdcchen@gmail.com>
2012-10-26 23:38:54 +0800

    123123123

--- protected/modules/mobile/views/comment/_one.php
+++ protected/modules/mobile/views/comment/_one.php
@@ -5,7 +5,7 @@
     <dd class="beta-comment-toolbar">
         <a class="beta-comment-reply" href="javascript:void(0);" data-url="<?php echo $comment->replyUrl;?>" rel="nofollow">回复</a>
         <a class="beta-comment-rating" href="javascript:void(0);" data-url="<?php echo $comment->supportUrl;?>" rel="nofollow">支持(<span class="beta-comment-join-nums"><?php echo $comment->up_score;?></span>)</a>
-        <a class="beta-comment-rating" href="javascript:void(0);" data-url="<?php echo $comment->againstUrl;?>" rel="nofollow">反对(<span class="beta-comment-join-nums"><?php echo $comment->down_score;?></span></a>
+        <a class="beta-comment-rating" href="javascript:void(0);" data-url="<?php echo $comment->againstUrl;?>" rel="nofollow">反对(<span class="beta-comment-join-nums"><?php echo $comment->down_score;?></span>)</a>
     </dd>
 </dl>
 <?php endforeach;?>
