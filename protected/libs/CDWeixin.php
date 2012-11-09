02eb56677079952bff09ae4a3a9c2ceead8fcc44
Dong Chen <cdcchen@gmail.com>
2012-11-09 16:29:22 +0800

    qefqwefwef

--- protected/libs/CDWeixin.php
+++ protected/libs/CDWeixin.php
@@ -89,7 +89,7 @@ abstract class CDWeixin
         $items = '';
         foreach ((array)$posts as $p) {
             if (is_array($p))
-                $items .= sprintf($itemTpl, $p['Title'], $p['Description'], $p['Picurl'], $p['Url']);
+                $items .= sprintf($itemTpl, $p['Title'], $p['Discription'], $p['PicUrl'], $p['Url']);
             else
                 throw new Exception('$posts 数据结构错误');
         }
