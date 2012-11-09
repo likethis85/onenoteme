913023a181f5c848fcb0d87f7aa5ac3359954f3c
Dong Chen <cdcchen@gmail.com>
2012-11-10 01:12:21 +0800

    ewfwefwef

--- protected/modules/member/views/comment/list.php
+++ protected/modules/member/views/comment/list.php
@@ -25,5 +25,5 @@
     </tbody>
 </table>
 <?php if ($pages):?>
-<div class="beta-pages"><?php $this->widget('CLinkPager', array('pages'=>$pages, 'skin'=>'bootstrap', 'htmlOptions'=>array('class'=>'pagination')));?></div>
+<div class="pagination"><?php $this->widget('CLinkPager', array('pages'=>$pages, 'skin'=>'bootstrap'));?></div>
 <?php endif;?>
