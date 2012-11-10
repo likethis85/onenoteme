54ef0c10d7e6920cb1eda9c1ff606df908682472
Dong Chen <cdcchen@gmail.com>
2012-11-10 13:05:20 +0800

    wefwefwef

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -58,6 +58,7 @@ _hmt && _hmt.push(['_setCustomVar', 1, 'guest', <?php echo (int)user()->isGuest;
     	    <a href="<?php echo aurl('site/bdmap');?>" target="_blank">网站地图</a>&nbsp;&nbsp;
     	    <a href="<?php echo aurl('site/links');?>" target="_blank">友情链接</a>&nbsp;&nbsp;
     	    <a href="<?php echo aurl('tag/list');?>" target="_blank">全部标签</a>
+    	    <a href="<?php echo CDBase::siteHomeUrl();?>" target="_blank">总共：<?php echo Post::allCount();?>篇</a>
         </p>
     	<p class="fright">笑死人不尝命&nbsp;<a href="#top">TOP</a></p>
     	<div class="clear"></div>
--- protected/views/post/grid_list.php
+++ protected/views/post/grid_list.php
@@ -1,7 +1,7 @@
 <?php if ($this->channel != CHANNEL_VIDEO):?>
 <div class="panel panel10 bottom10px">
     <ul class="fleft hot-keyword">
-        <li><span class="cred announce">24小时更新：<?php echo Post::todayUpdateCount();?>，总数：<?php echo Post::allCount();?>。QQ群：49401589</span></li>
+        <li><span class="cred announce">24小时更新：<?php echo Post::todayUpdateCount();?>篇。&nbsp;&nbsp;&nbsp;QQ群：49401589</span></li>
     </ul>
     <ul class="mode-switch fright">
         <li class="fall"><a href="<?php echo aurl($this->route, array('page'=>(int)$_GET['page'], 's'=>POST_LIST_STYLE_WATERFALL));?>">缩略图</a></li>
