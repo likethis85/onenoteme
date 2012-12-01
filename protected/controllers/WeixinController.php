a84c7ae5a73a85d83d7e516b00256dd48adc0b6d
Dong Chen <cdcchen@gmail.com>
2012-12-01 11:44:11 +0800

    ddd

--- protected/controllers/WeixinController.php
+++ protected/controllers/WeixinController.php
@@ -8,6 +8,27 @@ class WeixinController extends Controller
         $weixin->run();
         exit(0);
     }
+    
+    private function actionRandom()
+    {
+        $duration = 24 * 60 * 60;
+        $maxID = app()->getDb()->cache($duration)->createCommand()
+            ->select('max(id)')
+            ->from(TABLE_POST)
+            ->queryScalar();
+    
+        $randomID = mt_rand(0, $maxID);
+        $cmd = app()->getDb()->createCommand()
+            ->select('id')
+            ->from(TABLE_POST)
+            ->where(array('and', 'state = :enabled', 'channel_id = 0', 'id > :randomID'), array(':enabled' => POST_STATE_ENABLED, ':randomID' => $randomID));
+        $id = $cmd->queryScalar();
+    
+        if (empty($id))
+            echo 'null';
+    
+        echo $id;
+    }
 }
 
 
