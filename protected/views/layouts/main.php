45b97056c293aaf0767bc33f7736b5eebde20bb0
Dong Chen <cdcchen@gmail.com>
2013-01-05 19:52:47 +0800

    修改css

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -87,9 +87,9 @@ cs()->registerMetaTag('text/html; charset=utf-8', null, 'content-type')
     ->registerLinkTag('alternate', 'application/rss+xml', aurl('feed'), null, array('title'=>app()->name . ' » Feed'));
 
 YII_DEBUG || cs()->scriptMap = array(
-    'bootstrap.min.css' => sbu('styles/cd-all.min.css?t=2012122201'),
-    'cd-basic.css' => sbu('styles/cd-all.min.css?t=2012122201'),
-    'cd-main.css' => sbu('styles/cd-all.min.css?t=2012122201'),
+    'bootstrap.min.css' => sbu('styles/cd-all.min.css?t=2012010501'),
+    'cd-basic.css' => sbu('styles/cd-all.min.css?t=2012010501'),
+    'cd-main.css' => sbu('styles/cd-all.min.css?t=2012010501'),
     'bootstrap.min.js' => sbu('scripts/cd-all.min.js?t=2012122202'),
     'jquery.lazyload.min.js' => sbu('scripts/cd-all.min.js?t=2012122202'),
     'jquery.infinitescroll.min.js' => sbu('scripts/cd-all.min.js?t=2012122202'),
