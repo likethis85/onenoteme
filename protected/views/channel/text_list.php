b16a688e88510b41894f5a91760bd4964637bcca
Dong Chen <cdcchen@gmail.com>
2013-01-05 21:54:52 +0800

    给频道首页单独添加广告位

--- protected/views/channel/text_list.php
+++ protected/views/channel/text_list.php
@@ -2,7 +2,7 @@
     <?php $this->renderPartial('/post/grid_list', array('models'=>$models, 'pages'=>$pages));?>
 </div>
 <div class="fright cd-sidebar">
-    <?php $this->widget('CDAdvert', array('solt'=>'posts_list_sidebar_01'));?>
+    <?php $this->widget('CDAdvert', array('solt'=>'channel_home_sidebar_01'));?>
 	<div class="panel panel15"><?php $this->widget('CDHotTags', array('title'=>'热门标签'));?></div>
 </div>
 <div class="clear"></div>
