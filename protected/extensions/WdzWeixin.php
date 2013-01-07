60b1609b048631c438131a91809ecca16918ab4b
Dong Chen <cdcchen@gmail.com>
2013-01-07 19:45:44 +0800

    123123123

--- protected/extensions/WdzWeixin.php
+++ protected/extensions/WdzWeixin.php
@@ -5,10 +5,19 @@ class WdzWeixin extends CDWeixin
     {
         $hello = 'hello2bizuser';
         $input = trim($data->Content);
-        if (empty($input) || stripos($input, $hello) !== false)
-            $this->method0();
-        else
-            $this->random($data);
+        
+        if (is_numeric($input)) {
+            $method = 'method' . $input;
+            $result = false;
+            if (method_exists($this, $method)) {
+                $result = call_user_func(array($this, $method), $input);
+                if ($result === false)
+                    self::error();
+            }
+        }
+
+        $this->method0();
+        exit(0);
         
     }
     
@@ -24,68 +33,19 @@ class WdzWeixin extends CDWeixin
 //         file_put_contents(app()->runtimePath . '/wx2.txt', $log);
     }
     
-    private function method1()
+    private function method1($input)
     {
-        $text = '最冷笑话精选，每天分享笑话N枚，你的贴身开心果';
-        $posts = array(
-            array(
-                'Title' => '挖笑话，每日最冷笑话精选',
-                'Discription' => $text,
-                'PicUrl' => 'http://s.waduanzi.com/images/wx1.jpg',
-                'Url' => 'http://m.waduanzi.com/joke',
-            )
-        );
-        $xml = $this->outputNews($text, $posts);
-        header('Content-Type: application/xml');
-        echo $xml;
+        $this->nextJoke($input);
     }
     
-    private function method2()
+    private function method2($input)
     {
-        $text = '挖趣图 - 最搞笑的，最好玩的，最内涵的图片精选';
-        $posts = array(
-            array(
-                'Title' => '挖趣图 - 最内涵的图片精选',
-                'Discription' => $text,
-                'PicUrl' => 'http://s.waduanzi.com/images/wx2.jpg',
-                'Url' => 'http://m.waduanzi.com/lengtu',
-            )
-        );
-        $xml = $this->outputNews($text, $posts);
-        header('Content-Type: application/xml');
-        echo $xml;
+        $this->nextLengtu($input);
     }
     
-    private function method3()
+    private function method3($input)
     {
-        $text = '挖福利 - 最新最全的女明星写真、清纯校花、美女模特、正妹性感自拍';
-        $posts = array(
-            array(
-                'Title' => '挖福利 - 最新最全的美女模特',
-                'Discription' => $text,
-                'PicUrl' => 'http://s.waduanzi.com/images/wx3.jpg',
-                'Url' => 'http://m.waduanzi.com/girl',
-            )
-        );
-        $xml = $this->outputNews($text, $posts);
-        header('Content-Type: application/xml');
-        echo $xml;
-    }
-    
-    private function method9()
-    {
-        $text = '最新发布的段子，每日精品笑话连载。网罗互联网各种精品段子，各种糗事，各种笑话，各种秘密，各种经典语录，各种有趣的图片，各种漂亮mm校花模特正妹，应有尽有。烦了、累了、无聊了，就来挖段子逛一逛。"';
-        $posts = array(
-            array(
-                'Title' => '欢迎访问挖段子网手机版',
-                'Discription' => $text,
-                'PicUrl' => 'http://s.waduanzi.com/images/wx.jpg',
-                'Url' => 'http://m.waduanzi.com',
-            )
-        );
-        $xml = $this->outputNews($text, $posts);
-        header('Content-Type: application/xml');
-        echo $xml;
+        $this->nextGirl($input);
     }
     
     private function method0()
@@ -96,7 +56,7 @@ class WdzWeixin extends CDWeixin
         echo $xml;
     }
     
-    private function random($data)
+    private function nextJoke($data)
     {
         $wxid = $data->FromUserName;
         $lastID = app()->getDb()->createCommand()
@@ -132,8 +92,122 @@ class WdzWeixin extends CDWeixin
                 ->update(TABLE_USER_WEIXIN, $columns, 'wx_token = :wxid', array(':wxid' => $wxid));
         }
         
-        $row['content'] .= "\n\n回复 1 查看下一条\n回复 0 查看使用帮助\n\n喜欢我们就召唤好友添加'挖段子'或'waduanzi'为好友关注我们吧！";
+        $row['content'] .= self::helpInfo();
         $xml = $this->outputText($row['content']);
+        header('Content-Type: application/xml');
+        echo $xml;
+    }
+    
+    private function nextLengtu($data)
+    {
+        $wxid = $data->FromUserName;
+        $lastID = app()->getDb()->createCommand()
+            ->select('last_lengtu_pid')
+            ->from(TABLE_USER_WEIXIN)
+            ->where('wx_token = :wxid', array(':wxid'=>$wxid))
+            ->queryScalar();
+        
+        $cmd = app()->getDb()->createCommand()
+            ->select(array('id', 'content'))
+            ->from(TABLE_POST)
+            ->where(array('and', 'state = :enabled', 'channel_id = 0', 'id > :lastID'), array(':enabled' => POST_STATE_ENABLED, ':lastID' => (int)$lastID));
+        $row = $cmd->queryRow();
+        
+        if (empty($row['content'])) return ;
+        
+        if ($lastID === false) {
+            $columns = array(
+                'wx_token' => $wxid,
+                'create_time' => time(),
+                'last_time' => time(),
+                'last_lengtu_pid' => 0,
+            );
+            app()->getDb()->createCommand()
+                ->insert(TABLE_USER_WEIXIN, $columns);
+        }
+        else {
+            $columns = array(
+                'last_time' => time(),
+                'last_lengtu_pid' => (int)$row['id'],
+            );
+            app()->getDb()->createCommand()
+                ->update(TABLE_USER_WEIXIN, $columns, 'wx_token = :wxid', array(':wxid' => $wxid));
+        }
+        
+        $text = h($row['title']);
+        $posts = array(
+            array(
+                'Title' => $text,
+                'Discription' => mb_strimwidth($row['content'], 0, 100),
+                'PicUrl' => $row['thumbnail_pic'],
+                'Url' => aurl('mobile/post/show', array('id'=>$row['id'])),
+            )
+        );
+        $xml = $this->outputNews($text, $posts);
+        header('Content-Type: application/xml');
+        echo $xml;
+    }
+    
+    private function nextGirl($data)
+    {
+        $wxid = $data->FromUserName;
+        $lastID = app()->getDb()->createCommand()
+            ->select('last_girl_pid')
+            ->from(TABLE_USER_WEIXIN)
+            ->where('wx_token = :wxid', array(':wxid'=>$wxid))
+            ->queryScalar();
+        
+        $cmd = app()->getDb()->createCommand()
+            ->select(array('id', 'content'))
+            ->from(TABLE_POST)
+            ->where(array('and', 'state = :enabled', 'channel_id = 0', 'id > :lastID'), array(':enabled' => POST_STATE_ENABLED, ':lastID' => (int)$lastID));
+        $row = $cmd->queryRow();
+        
+        if (empty($row['content'])) return ;
+        
+        if ($lastID === false) {
+            $columns = array(
+                'wx_token' => $wxid,
+                'create_time' => time(),
+                'last_time' => time(),
+                'last_girl_pid' => 0,
+            );
+            app()->getDb()->createCommand()
+                ->insert(TABLE_USER_WEIXIN, $columns);
+        }
+        else {
+            $columns = array(
+                'last_time' => time(),
+                'last_girl_pid' => (int)$row['id'],
+            );
+            app()->getDb()->createCommand()
+                ->update(TABLE_USER_WEIXIN, $columns, 'wx_token = :wxid', array(':wxid' => $wxid));
+        }
+        
+        $text = h($row['title']);
+        $posts = array(
+            array(
+                'Title' => $text,
+                'Discription' => mb_strimwidth($row['content'], 0, 100),
+                'PicUrl' => $row['thumbnail_pic'],
+                'Url' => aurl('mobile/post/show', array('id'=>$row['id'])),
+            )
+        );
+        $xml = $this->outputNews($text, $posts);
+        header('Content-Type: application/xml');
+        echo $xml;
+    }
+    
+    private static function helpInfo()
+    {
+        $text = "\n\n回复 1 查看下一条\n回复 0 查看使用帮助\n\n喜欢我们就召唤好友添加'挖段子'或'waduanzi'为好友关注我们吧！";
+        return $text;
+    }
+    
+    private static function error()
+    {
+        $text = '系统接口整在升级中，请稍候再试。。。' . self::helpInfo();
+        $xml = $this->outputText($text);
         header('Content-Type: application/xml');
         echo $xml;
     }
