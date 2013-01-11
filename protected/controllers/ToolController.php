9f29285752f426a95a3a8ecb764440d94430fe20
Dong Chen <cdcchen@gmail.com>
2013-01-12 03:28:02 +0800

    添加王宝强表情制作器

---
+++ protected/controllers/ToolController.php
@@ -0,0 +1,62 @@
+<?php
+
+defined('ACCESS_KEY') || define('ACCESS_KEY', 'kn215onkjl');
+defined('SECRET_KEY') || define('SECRET_KEY', '4zx0ki4kmjzmy2wlh1340zyy5533yxy30kly2k1y');
+
+
+class ToolController extends Controller
+{
+    public function actionTaijiong()
+    {
+        $this->renderPartial('taijiong');
+    }
+    
+    public function actionMakepic()
+    {
+        $data = array();
+        if (strtolower($_SERVER['REQUEST_METHOD']) !== 'post') {
+            $data['errno'] = -1;
+            $data['error'] = '非法请求';
+            echo json_encode($data);
+            exit(0);
+        }
+        
+        $text1 = strip_tags($_POST['text1']);
+        $text2 = strip_tags($_POST['text2']);
+        $text3 = strip_tags($_POST['text3']);
+        
+        if (empty($text1) || empty($text2) || empty($text3)) {
+            $data['errno'] = -2;
+            $data['error'] = '三句台词都必须填写';
+            echo json_encode($data);
+            exit(0);
+        }
+        
+        try {
+            $picfile = sbp('images/originalpic.jpg');
+            $im = new CDImage();
+            $im->load($picfile);
+            
+            $font = Yii::getPathOfAlias('application.fonts') . DS . 'msyhb.ttf';
+            $im->text($text1, $font, 16, array(10, 218), array(255, 255, 255));
+            $im->text($text2, $font, 16, array(10, 450), array(255, 255, 255));
+            $im->text($text3, $font, 16, array(10, 690), array(255, 255, 255));
+            
+            $path = CDBase::makeUploadPath('taijiong');
+            $file = CDBase::makeUploadFileName();
+            $filename = $path['path'] . $file;
+            $im->saveAsJpeg($filename);
+            
+            $data['errno'] = 0;
+            $data['url'] = fbu($path['url'] . $im->filename());
+            echo json_encode($data);
+            exit(0);
+        }
+        catch (Exception $e) {
+            $data['errno'] = -3;
+            $data['error'] = '图片保存出错';
+            echo json_encode($data);
+            exit(0);
+        }
+    }
+}
---
+++ protected/views/tool/taijiong.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>王宝强《泰囧》超贱表情制作器</title>
+<link rel="stylesheet" type="text/css" href="<?php echo sbu('libs/bootstrap/css/bootstrap.min.css');?>" />
+<script type="text/javascript" src="<?php echo sbu('libs/jquery.min.js');?>"></script>
+<style type="text/css">
+.clear {clear:both; height:0; line-height:0; font-size:0;}
+.space10px {clear:both; height:10px; line-height:0; font-size:0;}
+.space15px {clear:both; height:15px; line-height:0; font-size:0;}
+.space20px {clear:both; height:20px; line-height:0; font-size:0;}
+
+.cdc-container {margin:15px;}
+.cdc-container .pull-right {width:300px;}
+.cdc-container .done-pic {text-align:center;}
+.cdc-container .done-pic img {width:409px;}
+form.taijiong {margin:0 auto;}
+form.taijiong textarea {height:50px; width:280px;}
+#result-tip {color:red; margin-top:15px;}
+</style>
+<script type="text/javascript">
+var picurl = '<?php echo sbu('images/originalpic.jpg');?>';
+</script>
+</head>
+<body>
+<div class="container-fluid cdc-container">
+    <div class="pull-right">
+        <?php echo CHtml::beginForm(aurl('tool/makepic'), 'post', array('class'=>'taijiong'));?>
+            <fieldset>
+                <legend>王宝强《泰囧》超贱表情制作器</legend>
+                <label>第&nbsp;2&nbsp;段台词</label>
+                <textarea name="text1"></textarea>
+                <label>第&nbsp;2&nbsp;段台词</label>
+                <textarea class="span5" name="text2"></textarea>
+                <label>第&nbsp;3&nbsp;段台词</label>
+                <textarea class="span5" name="text3"></textarea>
+                <div class="clear"></div>
+                <input type="button" class="btn btn-primary btn-block btn-large btn-make" value="贱一把" />
+                <div class="alert alert-error" id="result-tip">请输入 3 段台词</div>
+            </fieldset>
+        <?php echo CHtml::endForm();?>
+    </div>
+    <div class="done-pic">
+        <img src="<?php echo sbu('images/jiong_pic.jpg');?>" id="user-pic" />
+    </div>
+</div>
+
+<!-- Baidu Button BEGIN -->
+<script type="text/javascript" id="bdshare_js" data="type=slide&amp;img=0&amp;pos=left&amp;uid=541407" ></script>
+<script type="text/javascript" id="bdshell_js"></script>
+<script type="text/javascript">
+document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
+</script>
+<!-- Baidu Button END -->
+<!-- 将此代码放在适当的位置，建议在body结束前 -->
+<script id="bdimgshare_shell"></script>
+<script>
+var bdShare_config_imgshare = {
+	"type":"list"
+	,"size":"big"
+	,"pos":"top"
+	,"color":"white"
+	,"list":["qzone","tsina","tqq","renren","t163"]
+	,"uid":"541407"
+};
+document.getElementById("bdimgshare_shell").src="http://bdimg.share.baidu.com/static/js/imgshare_shell.js?cdnversion=" + Math.ceil(new Date()/3600000);
+</script>
+
+</body>
+</html>
+
+<script type="text/javascript">
+$(function(){
+	$('form.taijiong').on('click', '.btn-make', function(event){
+	    var text1 = $.trim($('textarea[name=text1]').val());
+	    var text2 = $.trim($('textarea[name=text2]').val());
+	    var text3 = $.trim($('textarea[name=text3]').val());
+	    if (text1.length == 0 || text2.length == 0 || text3.length == 0) {
+	    	$('#result-tip').html('3&nbsp;段台词都必须填写...');
+	    	return false;
+	    }
+		
+		var form = $('form.taijiong');
+		var url = form.attr('action');
+		var data = form.serialize();
+		var jqXhr = $.ajax(url, {
+			type: 'POST',
+			dataType: 'json',
+			data: data,
+			beforeSend: function(){
+				$('#result-tip').html('正在生成超贱图片...');
+			},
+		});
+
+		jqXhr.done(function(data, textStatus, xhr){
+			if (data.errno == 0) {
+				$('#user-pic').attr('src', data.url);
+				picurl = data.url;
+				$('#result-tip').html('超贱图片已经出生啦，赶紧分享给好友吧!');
+				document.title = '#王宝强超贱表情#' + text1 + '，' + text2 + '，' + text3;
+			}
+			else
+				$('#result-tip').html(data.error);
+		});
+		
+		jqXhr.fail(function(xhr, textStatus, errorThrown){
+			$('#result-tip').html(errorThrown);
+		});
+
+	});
+});
+</script>
+
