b025fcd9f37c229603751cb03c59a155cb81b4a1
Dong Chen <cdcchen@gmail.com>
2013-01-12 11:22:36 +0800

    添加页面编码

--- protected/views/tool/taijiong.php
+++ protected/views/tool/taijiong.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html xmlns:wb=“http://open.weibo.com/wb”>
 <head>
+<meta charset="utf-8">
 <title>王宝强《泰囧》超贱表情制作器</title>
 <meta name="keywords" content="王宝强超贱表情制作器,超贱表情制作器,王宝强《泰囧》超贱表情,王宝强超贱表情,超贱表情,王宝强神ps,王宝强ps" />
 <meta name="description" content="王宝强《泰囧》超贱表情制作器，轻轻松松制作王宝强超贱表情。" />
