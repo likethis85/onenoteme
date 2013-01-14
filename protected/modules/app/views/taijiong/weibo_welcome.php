db36fec919663416b0974e9b1348bb433bcda548
Dong Chen <cdcchen@gmail.com>
2013-01-14 21:53:46 +0800

    sadfadf

--- protected/modules/app/views/taijiong/weibo_welcome.php
+++ protected/modules/app/views/taijiong/weibo_welcome.php
@@ -19,7 +19,7 @@ body {margin:0; padding:0;}
 <script type="text/javascript">
 App.AuthDialog.show({
 	"client_id": '<?php echo TAIJIONG_WEIBO_APP_KEY;?>',
-	"redirect_uri": '<?php echo aurl('app/taijiong/appwindow');?>'
+	"redirect_uri": '<?php echo aurl('app/taijiong/weibowindow');?>'
 });
 </script>
 
