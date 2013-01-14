6308cb174b9ac9b584a46da829bad957f1a9526b
Dong Chen <cdcchen@gmail.com>
2013-01-14 22:49:04 +0800

    sadfasdfasdf

--- protected/modules/app/views/taijiong/weibo_welcome.php
+++ protected/modules/app/views/taijiong/weibo_welcome.php
@@ -1,15 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8">
 <title>未授权时的页面</title>
+<style type="text/css">
+body {margin:0; padding:0;}
+</style>
 <script src="http://tjs.sjs.sinajs.cn/t35/apps/opent/js/frames/client.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript">
 function pageloaded ()
 {
     App.AuthDialog.show({
     	client_id: '<?php echo TAIJIONG_WEIBO_APP_KEY;?>',
-    	redirect_uri: '<?php echo aurl('app/taijiong/weibo');?>',
+    	redirect_uri: 'http://apps.weibo.com/taijiong',
     	height: 120
     });
 }
