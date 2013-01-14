5b06f34bf0ff5eee04db94d15763de9e7f6d2913
Dong Chen <cdcchen@gmail.com>
2013-01-15 00:16:54 +0800

    dfsgdfsg

--- protected/modules/app/views/taijiong/weibo_welcome.php
+++ protected/modules/app/views/taijiong/weibo_welcome.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <title>未授权时的页面</title>
 <style type="text/css">
-body {margin:0; padding:0;}
+body {margin:0; padding:0; background:url(http://ww2.sinaimg.cn/large/61b3022etw1e0tjt3x35nj.jpg) no-repeat;}
 </style>
 <script src="http://tjs.sjs.sinajs.cn/t35/apps/opent/js/frames/client.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript">
