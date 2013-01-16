d0d4aecdd6c5e4f3517313f67de22f71692ac2f6
Dong Chen <cdcchen@gmail.com>
2013-01-16 14:40:13 +0800

    hrtrthrth

---
+++ protected/modules/app/views/taijiong/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html xmlns:wb="http://open.weibo.com/wb">
+<head>
+<meta charset="utf-8">
+<title>王宝强《泰囧》超贱表情制作器</title>
+<meta name="keywords" content="王宝强超贱表情制作器,超贱表情制作器,王宝强《泰囧》超贱表情,王宝强超贱表情,超贱表情,王宝强神ps,王宝强ps" />
+<meta name="description" content="王宝强《泰囧》超贱表情制作器，轻轻松松制作王宝强超贱表情。" />
+<script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=553903864" type="text/javascript" charset="utf-8"></script>
+<body>
+<wb:publish toolbar="face,image,topic" button_type="red" button_size="middle" default_text="#手机微博意见反馈#" ></wb:publish>
+</body>
+</html>
