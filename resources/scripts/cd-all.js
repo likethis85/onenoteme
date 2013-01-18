4311b390485b916bef5b6a07405798f3c9ed0314
Dong Chen <cdcchen@gmail.com>
2013-01-19 01:59:06 +0800

    cvbvbxcvbxcvb

--- resources/scripts/cd-all.js
+++ resources/scripts/cd-all.js
@@ -134,6 +134,7 @@ Waduanzi.PostComment = function(event) {
 			// post list valid
 			var commentButton = form.parents('.post-box').find('a.comment');
 			if (commentButton.length > 0) {
+				var commentBlock = form.parents('.comment-list');
 				commentButton.data('comments', commentBlock.html());
 			}
 		}
--- resources/scripts/cd-main.js
+++ resources/scripts/cd-main.js
@@ -104,6 +104,7 @@ Waduanzi.PostComment = function(event) {
 			// post list valid
 			var commentButton = form.parents('.post-box').find('a.comment');
 			if (commentButton.length > 0) {
+				var commentBlock = form.parents('.comment-list');
 				commentButton.data('comments', commentBlock.html());
 			}
 		}
