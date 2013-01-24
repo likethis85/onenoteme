ef8c3d396f814c996fb051d870e97a8c4b06ffbd
Dong Chen <cdcchen@gmail.com>
2013-01-24 13:19:09 +0800

    dfghdfghfgh

--- protected/modules/admin/config/params.php
+++ protected/modules/admin/config/params.php
@@ -4,23 +4,21 @@ return array(
      * 管理后台相关配置
     */
     // 文章列表每页显示的文章数量
-    'adminPostCountOfPage' => 20,
+    'adminPostCountOfPage' => 30,
     // 每页显示的评论数量
-    'adminCommentCountOfPage' => 20,
+    'adminCommentCountOfPage' => 25,
     // 每页显示的标签数量
-    'adminTagCountOfPage' => 20,
+    'adminTagCountOfPage' => 30,
     // 每页显示的主题数量
     'adminTopicCountOfPage' => 20,
     // 每页显示的分类数量
     'adminCategoryCountOfPage' => 20,
     // 每页显示的用户数量
-    'adminUserCountOfPage' => 20,
+    'adminUserCountOfPage' => 30,
     // 每页显示的评论数量
     'adminCommentCountOfPage' => 30,
     // 每页显示的文件数量
     'adminUploadFilesCountOfPage' => 30,
-    // 每页显示的专题数量
-    'adminSpecialCountOfPage' => 20,
     // 每页显示的链接数量
     'adminLinkCountOfPage' => 20,
         
