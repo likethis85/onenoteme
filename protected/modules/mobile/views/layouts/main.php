3d27abd1629d0ecc7ac1e6fb17bd8d3891756c5d
Dong Chen <cdcchen@gmail.com>
2013-01-27 16:45:37 +0800

    454545

--- protected/modules/mobile/views/layouts/main.php
+++ protected/modules/mobile/views/layouts/main.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="apple-mobile-web-app-status-bar-style" content="default" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <link rel="stylesheet" type="text/css" href="<?php echo sbu('libs/bootstrap/css/bootstrap.min.css');?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo sbu('styles/cd-mobile.css');?>" />
 </head>
 <body>
 <noscript><div id="noscript"><h2>Notice</h2><p>JavaScript is currently off.</p><p>Turn it on in browser settings to view this mobile website.</p></div></noscript>
