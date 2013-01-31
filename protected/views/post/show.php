b3aafe0b513dea0b5e56f4bfe6ff43729e33776b
Dong Chen <cdcchen@gmail.com>
2013-02-01 00:06:38 +0800

    dfvdfvdfv

--- protected/views/post/show.php
+++ protected/views/post/show.php
@@ -94,11 +94,13 @@
     <!-- 详情页侧边栏广告位开始 -->
     <?php $this->widget('CDAdvert', array('solt'=>'sidebar_post_detail_first'));?>
     <!-- 详情页侧边栏广告位结束 -->
+     <?php if (empty($post->bmiddlePic)):?>
     <div class="cdc-block">
         <!--cnzz tui-->
         <script  type="text/javascript" charset="utf-8"  src="http://tui.cnzz.net/cs.php?id=1000021168"></script>
         <!--cnzz tui-->
     </div>
+    <?php endif;?>
 	<div class="panel panel15"><?php $this->widget('CDHotTags', array('title'=>'热门标签'));?></div>
 </div>
 <div class="clear"></div>
