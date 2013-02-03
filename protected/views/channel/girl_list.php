0e282475246ca22be5b43b4f8fc50144e05bb793
Dong Chen <cdcchen@gmail.com>
2013-02-03 21:27:17 +0800

    mjmjm

--- protected/views/channel/girl_list.php
+++ protected/views/channel/girl_list.php
@@ -2,11 +2,6 @@
     <?php $this->renderPartial($list_view, array('models'=>$models, 'pages'=>$pages));?>
 </div>
 <div class="fright cd-sidebar">
-    <div class="cdc-block">
-        <!--cnzz tui-->
-        <script  type="text/javascript" charset="utf-8"  src="http://tui.cnzz.net/cs.php?id=1000021168"></script>
-        <!--cnzz tui-->
-    </div>
     <?php $this->widget('CDAdvert', array('solt'=>'channel_home_sidebar_01'));?>
 	<div class="panel panel15 bottom15px"><?php $this->widget('CDHotTags', array('title'=>'热门标签'));?></div>
     <?php $this->widget('CDAdvert', array('solt'=>'channel_home_sidebar_02'));?>
