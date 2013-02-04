b9f554f420cc0ff493bd408816f5085efbd9b19e
Dong Chen <cdcchen@gmail.com>
2013-02-04 15:16:39 +0800

    erbererreg

--- protected/views/post/line_list.php
+++ protected/views/post/line_list.php
@@ -6,6 +6,7 @@
     </ul>
     <ul class="mode-switch fright">
         <li class="fall"><a href="<?php echo aurl($this->route, array('page'=>(int)$_GET['page'], 's'=>POST_LIST_STYLE_WATERFALL));?>">瀑布流</a></li>
+        <li class="grid"><a href="<?php echo aurl($this->route, array('page'=>(int)$_GET['page'], 's'=>POST_LIST_STYLE_GRID));?>">表格</a></li>
         <li class="list on"><a href="<?php echo aurl($this->route, array('page'=>(int)$_GET['page'], 's'=>POST_LIST_STYLE_LINE));?>">列表</a></li>
     </ul>
     <div class="clear"></div>
