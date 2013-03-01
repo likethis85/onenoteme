22cb8f58fe07d7b30038832334949e12ddcab583
Dong Chen <cdcchen@gmail.com>
2013-03-01 23:23:35 +0800

    优化微信接口

--- protected/extensions/WdzWeixin.php
+++ protected/extensions/WdzWeixin.php
@@ -1,32 +1,50 @@
 <?php
 class WdzWeixin extends CDWeixin
 {
+    const POST_JOKE_CONTENT_MIN_LEN = 20;
+    
     public function processRequest()
     {
+        if ($this->isTextMsg())
+            $this->textMsgRequest();
+        else
+            $this->unSupportMsgType();
+    
+        exit(0);
+    }
+    
+    private function textMsgRequest()
+    {
         $hello = 'hello2bizuser';
-        $input = trim($this->_data->Content);
-        
-        if (strtolower($input) == $hello) {
+        $input = strtolower(trim($this->_data->Content));
+    
+        if ($input == $hello) {
             $this->welcome();
             exit(0);
         }
         
-        if ($this->isTextMsg() && is_numeric($input)) {
-            $method = 'method' . $input[0]; // 取第一个数字
+        if (is_numeric($input)) {
+            $method = 'method_' . $input[0]; // 取第一个数字
             $result = false;
             if (method_exists($this, $method)) {
                 if (false === call_user_func(array($this, $method)))
                     self::error();
             }
             else
-                $this->method0();
-            
+                $this->method_0();
+        
             exit(0);
         }
+    
+        $method = 'method_' . strtolower($input);
+        if (method_exists($this, $method)) {
+            if (call_user_func(array($this, $method)) === false)
+                $this->error();
+        }
+        elseif (mb_strlen($input, app()->charset) > self::POST_JOKE_CONTENT_MIN_LEN)
+            ;
         else
-            $this->unSupportMsgType();
-        
-        exit(0);
+            $this->method_0();
     }
     
     public function errorHandler($errno, $error, $file = '', $line = 0)
@@ -57,27 +75,27 @@ class WdzWeixin extends CDWeixin
         echo $xml;
     }
     
-    private function method1()
+    private function method_1()
     {
         $this->nextJoke();
     }
     
-    private function method2()
+    private function method_2()
     {
         $this->nextLengtu();
     }
     
-    private function method3()
+    private function method_3()
     {
         $this->nextGirl();
     }
     
-    private function method4()
+    private function method_4()
     {
         $this->nextVideo();
     }
     
-    private function method0()
+    private function method_0()
     {
         $text = '您有推荐的冷笑话或、搞笑图片或有意思的视频欢迎直接微信投稿，与大家一起分享哟～' . self::helpInfo();
         $xml = $this->outputText($text);
