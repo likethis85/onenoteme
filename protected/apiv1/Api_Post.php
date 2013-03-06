96cd2b653aac2509edfa69b28cdcb1908f59d089
Dong Chen <cdcchen@gmail.com>
2013-03-06 09:30:45 +0800

    Update Api_Post.php

--- protected/apiv1/Api_Post.php
+++ protected/apiv1/Api_Post.php
@@ -299,7 +299,7 @@ class Api_Post extends ApiBase
     	$post->state = POST_STATE_DISABLED;
         $post->up_score = mt_rand(150, 500);
         $post->down_score = mt_rand(10, 50);
-        $post->view_nums = mt_rand(500, 100);
+        $post->view_nums = mt_rand(500, 1000);
     	$post->thumbnail_pic = $post->bmiddle_pic = '';
     	$post->original_pic = $params['pic'];
     	
