b6c6b3231fdff903bf02ba68ef5f2c450a960872
Dong Chen <cdcchen@gmail.com>
2013-03-22 17:49:33 +0800

    Update params_product.php

--- protected/config/params_product.php
+++ protected/config/params_product.php
@@ -20,8 +20,8 @@ return array(
         CHANNEL_DUANZI => '挖笑话',
         CHANNEL_LENGTU => '挖趣图',
         CHANNEL_GIRL => '挖女神',
-        CHANNEL_VIDEO => '挖视频',
-        CHANNEL_MOVIE => '挖电影',
+        CHANNEL_VIDEO => '挖短片',
+        CHANNEL_GHOSTSTORY => '挖鬼故事',
     ),
 
     /*
