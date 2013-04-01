07b8755b03bba004592b265e815f611806b4e9c6
Dong Chen <cdcchen@gmail.com>
2013-04-01 20:31:58 +0800

    kjhgfds

--- protected/config/params_product.php
+++ protected/config/params_product.php
@@ -84,9 +84,9 @@ return array(
     'relative_qqt_accounts' => '',
 
     /* cache setting */
-    'mobile_post_list_cache_expire' => 60 * 60,
-    'mobile_comment_list_cache_expire' => 10,
-    'mobile_post_show_cache_expire' => 10,
+    'mobile_post_list_cache_expire' => 60,
+    'mobile_comment_list_cache_expire' => 30,
+    'mobile_post_show_cache_expire' => 30,
         
     'wdz_weixin_account_name' => '挖段子搞笑',
     'wdz_weixin_account_id' => 'gh_9261dce78e9f',
