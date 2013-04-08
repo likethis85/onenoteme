15c7fd2e72a3be9078f8a9437a5e0dee259b9b91
Dong Chen <cdcchen@gmail.com>
2013-04-09 01:38:34 +0800

    oiugfd

--- resources/scripts/cd-all.js
+++ resources/scripts/cd-all.js
@@ -544,7 +544,7 @@ Waduanzi.setPrevNextButtonPosition = function() {
     var img = $('.post-picture.thumbbox img');
     var boxtop = $('.post-picture').position().top;
     var buttontop = boxtop + ((img.height() > 150) ? 100 : img.height() / 2);
-    var scrollMax = boxtop + img.height() - buttontop - 100 - 47; // 100是离图片底部的距离，47是图标的高度
+    var scrollMax = boxtop + img.height() - buttontop - 47; // 47是图标的高度
     if ($(document).scrollTop() < scrollMax) {
 	    button.css('position', 'fixed');
 	    button.css('top', buttontop);
