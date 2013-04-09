958ce09ad87e6472566cc8f717f5532cd452ca62
Dong Chen <cdcchen@gmail.com>
2013-04-09 22:46:17 +0800

    jhgfd

--- protected/views/post/mixed_list.php
+++ protected/views/post/mixed_list.php
@@ -40,7 +40,7 @@ $(function(){
     	var itemCount = $('.waterfall-item').length;
     	var manual = $('#manual-load');
     	container.masonry({
-    		gutterWidth: 10,
+    		gutterWidth: 15,
             itemSelector: '.waterfall-item'
         });
         
