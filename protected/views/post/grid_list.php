2cd3126d9ae8cf2125fa1f292e1377277d0279b7
Dong Chen <cdcchen@gmail.com>
2013-04-18 10:50:59 +0800

    Update grid_list.php

--- protected/views/post/grid_list.php
+++ protected/views/post/grid_list.php
@@ -4,9 +4,9 @@
         <li><span class="cred announce">24小时更新：<?php echo Post::todayUpdateCount();?>篇。&nbsp;&nbsp;&nbsp;QQ群：49401589</span></li>
     </ul>
     <ul class="mode-switch fright">
-        <li class="fall"><a href="<?php echo aurl($this->route, array('page'=>(int)$_GET['page'], 's'=>POST_LIST_STYLE_WATERFALL));?>">瀑布流</a></li>
-        <li class="grid on"><a href="<?php echo aurl($this->route, array('page'=>(int)$_GET['page'], 's'=>POST_LIST_STYLE_GRID));?>">表格</a></li>
-        <li class="list"><a href="<?php echo aurl($this->route, array('page'=>(int)$_GET['page'], 's'=>POST_LIST_STYLE_LINE));?>">列表</a></li>
+        <li class="fall"><a href="<?php echo CDBase::fallStyleUrl($this);?>">瀑布流</a></li>
+        <li class="grid on"><a href="<?php echo CDBase::gridStyleUrl($this);?>">表格</a></li>
+        <li class="list"><a href="<?php echo CDBase::lineStyleUrl($this);?>">列表</a></li>
     </ul>
     <div class="clear"></div>
 </div>
