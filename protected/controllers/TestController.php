a31e6054771ea08e70aad3dc06c15cff3161d5f9
Dong Chen <cdcchen@gmail.com>
2013-04-28 22:25:24 +0800

    nrnrnrn

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -1,7 +1,7 @@
 <?php
 class TestController extends Controller
 {
-    public function init1()
+    public function init()
     {
         $this->redirect('/');
         exit(0);
@@ -9,42 +9,6 @@ class TestController extends Controller
     
     public function actionIndex($page = 1)
     {
-        $count = 2000;
-        $page = (int)$page;
-        $page = ($page < 1) ? 1 : $page;
-        $offset = ($page - 1) * $count;
-        $cmd = app()->getDb()->createCommand()
-            ->from(TABLE_POST)
-            ->select(array('id', 'original_pic', 'create_time', 'user_id', 'create_ip', 'title', 'original_width', 'original_height', 'original_frames'))
-            ->where(array('and', 'channel_id = :channelID', 'id <= 41487'), array(':channelID'=>CHANNEL_GIRL))
-            ->order(array('id asc'))
-            ->limit($count)
-            ->offset($offset);
-        
-        $rows = $cmd->queryAll();
-        
-        foreach ($rows as $row) {
-            $upload = new Upload();
-            $upload->post_id = $row['id'];
-            $upload->file_type = Upload::TYPE_IMAGE;
-            $upload->url = $row['original_pic'];
-            $upload->create_time = $row['create_time'];
-            $upload->create_ip = $row['create_ip'];
-            $upload->width = $row['original_width'];
-            $upload->height = $row['original_height'];
-            $upload->frames = $row['original_frames'];
-            $upload->desc = $row['title'];
-            $upload->user_id = $row['user_id'];
-            
-            $result = $upload->save();
-            if ($result)
-                echo $row['id'] . ' OK<br />';
-            else {
-                echo $row['id'] . ' ERROR<br />';
-                break;
-            }
-        }
-        echo count($rows);
         exit;
     }
     
