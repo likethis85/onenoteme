cf02dac8c753e64063236f98416cafa24507fd1e
Dong Chen <cdcchen@gmail.com>
2013-04-28 14:52:40 +0800

    Update FeedController.php

--- protected/controllers/FeedController.php
+++ protected/controllers/FeedController.php
@@ -108,7 +108,7 @@ class FeedController extends Controller
     private static function fetchPosts(CDbCriteria $criteria)
     {
         $criteria->select = array('t.id', 't.title', 't.original_pic', 't.content', 't.create_time', 't.original_frames');
-        $criteria->order = array('t.create_time desc', 't.id desc');
+        $criteria->order = 't.create_time desc, t.id desc';
         $criteria->limit = self::POST_COUNT;
             
         $models = Post::model()->findAll($criteria);
