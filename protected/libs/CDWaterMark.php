ac9fb7b1ef200acf9f308e233ae8a8edd78dfb1e
Dong Chen <cdcchen@gmail.com>
2013-05-02 14:47:11 +0800

    Update CDWaterMark.php

--- protected/libs/CDWaterMark.php
+++ protected/libs/CDWaterMark.php
@@ -1,14 +1,14 @@
 <?php
 class CDWaterMark
 {
-    const POSITION_TOP_LEFT = 1;
-    const POSITION_TOP_CENTER = 2;
-    const POSITION_TOP_RIGHT = 3;
-    const POSITION_RIGHT_MIDDLE = 4;
-    const POSITION_BOTTOM_RIGHT = 5;
-    const POSITION_BOTTOM_CENTER = 6;
-    const POSITION_BOTTOM_LEFT = 7;
-    const POSITION_LEFT_MIDDLE = 8;
+    const POS_TOP_LEFT = 1;
+    const POS_TOP_CENTER = 2;
+    const POS_TOP_RIGHT = 3;
+    const POS_RIGHT_MIDDLE = 4;
+    const POS_BOTTOM_RIGHT = 5;
+    const POS_BOTTOM_CENTER = 6;
+    const POS_BOTTOM_LEFT = 7;
+    const POS_LEFT_MIDDLE = 8;
     
     public $position;
 }
