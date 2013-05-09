4fa3b47897abce847781daf94dbcd3e20e03341f
Dong Chen <cdcchen@gmail.com>
2013-05-09 14:20:36 +0800

    Update posts.php

--- protected/modules/mobile/views/channel/posts.php
+++ protected/modules/mobile/views/channel/posts.php
@@ -1,1 +1,7 @@
-<?php $this->renderPartial('/post/list', array('models'=>$models, 'pages'=>$pages));?>
+<script type="text/javascript">
+<!--
+_hmt && _hmt.push(['_setCustomVar', 2, 'channel_id', <?php echo $this->channel;?>, 3]);
+//-->
+</script>
+
+<?php $this->renderPartial('/post/list', array('models'=>$models, 'pages'=>$pages, 'channel'=>$this->channel));?>
