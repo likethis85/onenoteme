80abc17390f06a3edcee21d999ba7a3ea6d398a1
Dong Chen <cdcchen@gmail.com>
2013-05-09 13:28:29 +0800

    Update main.php

--- protected/modules/mobile/views/layouts/main.php
+++ protected/modules/mobile/views/layouts/main.php
@@ -27,13 +27,7 @@ _hmt && _hmt.push(['_setCustomVar', 1, 'guest', <?php echo (int)user()->isGuest;
     		<li>|</li>
     		<li<?php echo ($this->channel===CHANNEL_LENGTU) ? ' class="active"' : '';?>><a href="<?php echo aurl('mobile/channel/lengtu');?>">挖趣图</a></li>
     		<li>|</li>
-    		<li<?php echo ($this->channel===CHANNEL_GIRL) ? ' class="active"' : '';?>><a href="<?php echo aurl('mobile/channel/girl');?>">挖女神</a></li>
-    		<li>|</li>
-    		<li<?php echo ($this->channel===CHANNEL_GHOSTSTORY) ? ' class="active"' : '';?>><a href="<?php echo aurl('mobile/channel/ghost');?>">鬼故事</a></li>
-    		<!--
-    		<li>|</li>
-    		<li<?php echo ($this->channel===CHANNEL_VIDEO) ? ' class="active"' : '';?>><a href="<?php echo aurl('mobile/channel/video');?>">挖视频</a></li>
-    		 -->
+    		<li<?php echo ($this->channel===CHANNEL_GHOSTSTORY) ? ' class="active"' : '';?>><a href="<?php echo aurl('mobile/channel/ghost');?>">挖鬼故事</a></li>
     	</ul>
     </div>
 </div>
