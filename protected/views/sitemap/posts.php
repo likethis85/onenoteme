289b929eebd3583de94166074eee7a7f932f65a6
Dong Chen <cdcchen@gmail.com>
2013-05-10 13:08:19 +0800

    Update posts.php

--- protected/views/sitemap/posts.php
+++ protected/views/sitemap/posts.php
@@ -3,7 +3,7 @@
 <?php foreach ((array)$posts as $p):?>
 <url>
     <loc><?php echo aurl('post/show', array('id'=>$p['id']));?></loc>
-    <priority>0.60</priority>
+    <priority>0.90</priority>
 </url>
 <?php endforeach;?>
 </urlset>
