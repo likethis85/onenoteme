e00c3a803d21911d6a3d6641b7be05582e443dc7
Dong Chen <cdcchen@gmail.com>
2013-05-10 13:20:42 +0800

    Update and rename channels to channels.php

--- protected/views/sitemap/channels.php
+++ protected/views/sitemap/channels.php
@@ -1,23 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
+    <loc><?php echo aurl('site/index');?></loc>
+    <priority>1.0</priority>
+</url>
+<url>
     <loc><?php echo aurl('channel/joke');?></loc>
-    <priority>0.60</priority>
+    <priority>1.0</priority>
 </url>
 <url>
     <loc><?php echo aurl('channel/lengtu');?></loc>
-    <priority>0.60</priority>
+    <priority>1.0</priority>
 </url>
 <url>
     <loc><?php echo aurl('channel/girl');?></loc>
-    <priority>0.60</priority>
+    <priority>1.0</priority>
 </url>
 <url>
     <loc><?php echo aurl('channel/video');?></loc>
-    <priority>0.60</priority>
+    <priority>1.0</priority>
 </url>
 <url>
     <loc><?php echo aurl('channel/ghost');?></loc>
-    <priority>0.60</priority>
+    <priority>1.0</priority>
 </url>
 </urlset>
