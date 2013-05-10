874dfb7dd6850fc8e884421ff297ecd404897826
Dong Chen <cdcchen@gmail.com>
2013-05-10 11:46:08 +0800

    Update SitemapController.php

--- protected/controllers/SitemapController.php
+++ protected/controllers/SitemapController.php
@@ -12,32 +12,64 @@ class SitemapController extends Controller
         $this->renderPartial('index');
     }
     
+    public function actionArchives($date)
+    {
+        $this->fetchDatePosts($date);
+    }
+    
+    public function actionChannels()
+    {
+        $this->renderPartial('channels');
+    }
+    
     public function actionJoke()
     {
-        $this->fetchPosts(CHANNEL_DUANZI);
+        $this->fetchChannelPosts(CHANNEL_DUANZI);
     }
     
     public function actionLengtu()
     {
-        $this->fetchPosts(CHANNEL_LENGTU);
+        $this->fetchChannelPosts(CHANNEL_LENGTU);
     }
     
     public function actionGirl()
     {
-        $this->fetchPosts(CHANNEL_GIRL);
+        $this->fetchChannelPosts(CHANNEL_GIRL);
     }
     
     public function actionVideo()
     {
-        $this->fetchPosts(CHANNEL_VIDEO);
+        $this->fetchChannelPosts(CHANNEL_VIDEO);
     }
     
     public function actionGhost()
     {
-        $this->fetchPosts(CHANNEL_GHOSTSTORY);
+        $this->fetchChannelPosts(CHANNEL_GHOSTSTORY);
     }
     
-    private function fetchPosts($channel = null, $duration = 600)
+    private function fetchChannelPosts($channel = null, $duration = 600)
+    {
+        $conditions = 'state = :enabled';
+        $params = array(':enabled' => POST_STATE_ENABLED);
+        if ($channel !== null) {
+            $conditions = array('and', 'channel_id = :channelID', $conditions);
+            $params[':channelID'] = (int)$channel;
+        }
+        $cmd = app()->getDb()->cache($duration)->createCommand()
+            ->select('id')
+            ->from(TABLE_POST)
+            ->where($conditions, $params)
+            ->order('id desc')
+            ->limit(10000);
+        $posts = $cmd->queryAll();
+        $this->renderPartial('posts', array(
+            'posts' => $posts
+        ));
+        app()->end();
+        exit(0);
+    }
+    
+    private function fetchDatePosts($date = null, $duration = 600)
     {
         $conditions = 'state = :enabled';
         $params = array(':enabled' => POST_STATE_ENABLED);
