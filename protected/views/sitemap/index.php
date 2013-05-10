79a6100061b35f357bbc94c30347726de9e45437
Dong Chen <cdcchen@gmail.com>
2013-05-10 13:26:30 +0800

    Update index.php

--- protected/views/sitemap/index.php
+++ protected/views/sitemap/index.php
@@ -2,23 +2,31 @@
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
 <sitemap>
-<loc>http://www.waduanzi.com/sitemap/joke</loc>
+<loc><?php echo aurl('sitemap/channels');?></loc>
 </sitemap>
 
 <sitemap>
-<loc>http://www.waduanzi.com/sitemap/lengtu</loc>
+<loc><?php echo aurl('sitemap/tags');?></loc>
 </sitemap>
 
 <sitemap>
-<loc>http://www.waduanzi.com/sitemap/girl</loc>
+<loc><?php echo aurl('sitemap/joke');?></loc>
 </sitemap>
 
 <sitemap>
-<loc>http://www.waduanzi.com/sitemap/video</loc>
+<loc><?php echo aurl('sitemap/lengtu');?></loc>
 </sitemap>
 
 <sitemap>
-<loc>http://www.waduanzi.com/sitemap/ghost</loc>
+<loc><?php echo aurl('sitemap/girl');?></loc>
+</sitemap>
+
+<sitemap>
+<loc><?php echo aurl('sitemap/video');?></loc>
+</sitemap>
+
+<sitemap>
+<loc><?php echo aurl('sitemap/ghost');?></loc>
 </sitemap>
 
 </sitemapindex>
