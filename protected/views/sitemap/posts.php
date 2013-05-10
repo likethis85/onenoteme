2fed8929367bffdff4fe2641449576c6b742ea19
Dong Chen <cdcchen@gmail.com>
2013-05-10 11:09:22 +0800

    Create posts.php

---
+++ protected/views/sitemap/posts.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?php foreach ((array)$posts as $p):?>
+<url>
+    <loc><?php echo aurl('post/show', array('id'=>$p['id']));?></loc>
+    <priority>0.60</priority>
+</url>
+<?php endforeach;?>
+</urlset>
