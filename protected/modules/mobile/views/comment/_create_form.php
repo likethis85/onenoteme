ac251c22abdf7199ba9d7144eb3a9b819f4c360e
Dong Chen <cdcchen@gmail.com>
2013-05-26 22:06:40 +0800

    把提交修改为“留下我的节操”

--- protected/modules/mobile/views/comment/_create_form.php
+++ protected/modules/mobile/views/comment/_create_form.php
@@ -2,7 +2,7 @@
 <?php echo CHtml::form(aurl('mobile/comment/create'),  'post', array('class'=>'beta-comment-form', 'id'=>'comment-form'));?>
     <?php echo CHtml::activeHiddenField($comment, 'post_id');?>
     <?php echo CHtml::activeTextArea($comment, 'content', array('class'=>'comment-content', 'minlen'=>param('comment_min_length')));?>
-    <button type="submit" class="btn btn-block btn-primary" data-toggle="toggle" data-loading-text="发布中...">提交</button>
+    <button type="submit" class="btn btn-block btn-primary" data-toggle="toggle" data-loading-text="发布中...">留下我的节操</button>
 <?php echo CHtml::endForm();?>
 
 <script type="text/javascript">
