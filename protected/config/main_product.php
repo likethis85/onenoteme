570b8b7d27ab570c3e075daf0a5e46a35afa057c
Dong Chen <cdcchen@gmail.com>
2013-06-07 21:25:46 +0800

    44444

--- protected/config/main_product.php
+++ protected/config/main_product.php
@@ -147,7 +147,6 @@ return array(
                 'http://rest.waduanzi.com/post/show/<post_id:\d+>' => 'rest/post/show',
                 'http://rest.waduanzi.com/user/show/<user_id:\d+>' => 'rest/user/show',
                 'http://rest.waduanzi.com/comment/show/<post_id:\d+>' => 'rest/comment/show',
-                'http://rest.waduanzi.com/comment/<_a:(report|support)>/<comment_id:\d+>' => 'rest/comment/<_a>',
                 'http://rest.waduanzi.com/<_c>/<_a>' => 'rest/<_c>/<_a>',
             ),
         ),
