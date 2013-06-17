971dd0962a176540b254bbcc545431e61a88cc34
Dong Chen <cdcchen@gmail.com>
2013-06-17 11:38:11 +0800

    33333

--- protected/modules/rest/controllers/PostController.php
+++ protected/modules/rest/controllers/PostController.php
@@ -165,7 +165,14 @@ class PostController extends RestController
         $this->output($data);
     }
     
-    
+    public function actionRandom()
+    {
+        $criteria = new CDbCriteria();
+        $criteria->order = 't.create_time desc';
+        $criteria->addColumnCondition(array('t.media_type'=>MEDIA_TYPE_TEXT, 't.state' => POST_STATE_ENABLED));
+        $model = ApiPost::model()->find($criteria);
+        $this->output(CDDataFormat::formatPost($model));
+    }
     
     
     
