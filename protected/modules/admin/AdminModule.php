36da31f7751c71909cd8ba0dac785238f58c1d4b
Dong Chen <cdcchen@gmail.com>
2013-06-27 00:22:00 +0800

    rthrthrth

--- protected/modules/admin/AdminModule.php
+++ protected/modules/admin/AdminModule.php
@@ -36,3 +36,19 @@ class AdminModule extends CWebModule
 			return false;
 	}
 }
+
+if (!function_exists('getallheaders'))
+{
+    function getallheaders()
+    {
+        $headers = array();
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headerName = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
+                $headers[$headerName] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
