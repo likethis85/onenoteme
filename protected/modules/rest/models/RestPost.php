28c005c10b3051a7114a5a73b0742ddc8c82de78
Dong Chen <cdcchen@gmail.com>
2013-08-26 00:16:37 +0800

    ererergerg

--- protected/modules/rest/models/RestPost.php
+++ protected/modules/rest/models/RestPost.php
@@ -45,7 +45,7 @@ class RestPost extends Post
     {
         $html = '<!doctype html><html><head><meta charset="utf-8" /><title>' . $this->title . '</title></head><body>';
         $html .= '<h1>' . $this->title . '</h1>';
-        $html .= sprintf('<p id="time-author">%s&nbsp;&nbsp;%s</p>', $this->getCreateTime(), $this->getAuthorName());
+        $html .= sprintf('<p class="timeauthor">%s&nbsp;&nbsp;%s</p>', $this->getCreateTime(), $this->getAuthorName());
         
         $content = strip_tags($this->content, '<p><b><strong><span><img><br>');
         $matches = array();
