8f920cb3f5f4aeafccbfc81cb0f51acb084b0d45
Dong Chen <cdcchen@gmail.com>
2013-09-04 18:37:56 +0800

    bbbb

--- protected/controllers/AppsController.php
+++ protected/controllers/AppsController.php
@@ -3,6 +3,9 @@ class AppsController extends Controller
 {
     public function actionIndex()
     {
+        $this->setPageTitle('挖段子官方应用，挖段子iPhone应用，挖段子Android应用');
+        $this->setKeywords('挖段子官方应用,挖段子iPhone应用,挖段子Android应用');
+        $this->setDescription('挖段子官方应用，挖段子iPhone应用，挖段子Android应用');
         $this->layout = 'blank';
         $this->render('wdz');
     }
