12d25343d903fe4b6993ba7666cce5499a2fb6f5
Dong Chen <cdcchen@gmail.com>
2013-09-16 04:03:47 +0800

    wgwegewg

--- protected/modules/mobile/views/comment/_create_form.php
+++ protected/modules/mobile/views/comment/_create_form.php
@@ -1,12 +1,14 @@
 <div class="alert beta-alert beta-alert-message" id="beta-create-message" data-dismiss="alert"><a class="close" href="javascript:void(0);">&times;</a><span class="text"></span></div>
 <?php echo CHtml::form(aurl('mobile/comment/create'),  'post', array('class'=>'beta-comment-form', 'id'=>'comment-form'));?>
     <?php echo CHtml::activeHiddenField($comment, 'post_id');?>
-    <?php echo CHtml::activeTextArea($comment, 'content', array('class'=>'comment-content', 'minlen'=>param('comment_min_length')));?>
+    <?php echo CHtml::activeTextArea($comment, 'content', array('class'=>'comment-content', 'id'=>'comment-content', 'minlen'=>param('comment_min_length')));?>
     <button type="submit" class="btn btn-block btn-primary" data-toggle="toggle" data-loading-text="发布中...">留下我的节操</button>
 <?php echo CHtml::endForm();?>
 
 <script type="text/javascript">
 $(function(){
+	if (window.location.hash.length > 0)
+		$('#comment-content').focus();
 	$(document).on('submit', '.beta-comment-form', BetaComment.create);
 	$(document).on('blur', '.beta-comment-form .comment-content', BetaComment.contentValidate);
 });
