b78d8db5abecbe628a520cbe872958371169d6d7
Dong Chen <cdcchen@gmail.com>
2013-09-16 03:39:01 +0800

    ergererg

--- protected/modules/admin/controllers/DeviceController.php
+++ protected/modules/admin/controllers/DeviceController.php
@@ -6,6 +6,8 @@ class DeviceController extends AdminController
         $udid = trim($udid);
         $model = AdminMobileDevice::model()->findByPk($udid);
         
+        echo '<pre>';
         CVarDumper::dump($model);
+        echo '</pre>';
     }
 }
