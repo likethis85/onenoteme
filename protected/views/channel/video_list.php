c1d23f170515566935a028c33b9351f05d79a5e0
Dong Chen <cdcchen@gmail.com>
2013-09-18 10:27:15 +0800

    wefwefewf

--- protected/views/channel/video_list.php
+++ protected/views/channel/video_list.php
@@ -11,8 +11,8 @@ _hmt && _hmt.push(['_setCustomVar', 2, 'channel_id', <?php echo $this->channel;?
 <div class="fright cd-sidebar">
     <?php $this->widget('CDAdvert', array('solt'=>'channel_home_sidebar_01'));?>
     <!-- 56.com榜单 -->
-    <div class="cdc-block">
-        <iframe frameborder="0" id="customMoudle" name="customMoudle" width="300" height="260" src="http://s1.56img.com/style/i/admin/v4/tpl/gq_share/v1/share_layer_list.html?style=rank_tags&title=%E6%8C%96%E8%A7%86%E9%A2%91%E7%83%AD%E9%97%A8%E6%8E%92%E8%A1%8C%E6%A6%9C&width=300&height=260&border=false&usrId=r239612568"></iframe>
+    <div class="cdc-block acenter">
+        <iframe frameborder="0" id="customMoudle" name="customMoudle" width="276" height="260" src="http://s1.56img.com/style/i/admin/v4/tpl/gq_share/v1/share_layer_list.html?style=rank_tags&title=%E6%8C%96%E8%A7%86%E9%A2%91%E7%83%AD%E9%97%A8%E6%8E%92%E8%A1%8C%E6%A6%9C&width=276&height=260&border=false&usrId=r239612568"></iframe>
     </div>
     <?php $this->widget('CDAdvert', array('solt'=>'channel_home_sidebar_02'));?>
 	<div class="panel panel15 bottom15px"><?php $this->widget('CDHotTags', array('title'=>'热门标签'));?></div>
--- protected/views/post/show.php
+++ protected/views/post/show.php
@@ -95,8 +95,8 @@ _hmt && _hmt.push(['_setCustomVar', 2, 'channel_id', <?php echo (int)$this->chan
     </div>
     
     <!-- 56.com榜单 -->
-    <div class="cdc-block">
-        <iframe frameborder="0" id="customMoudle" name="customMoudle" width="300" height="260" src="http://s1.56img.com/style/i/admin/v4/tpl/gq_share/v1/share_layer_list.html?style=rank_tags&title=%E6%8C%96%E8%A7%86%E9%A2%91%E7%83%AD%E9%97%A8%E6%8E%92%E8%A1%8C%E6%A6%9C&width=300&height=260&border=false&usrId=r239612568"></iframe>
+    <div class="cdc-block acenter">
+        <iframe frameborder="0" id="customMoudle" name="customMoudle" width="276" height="260" src="http://s1.56img.com/style/i/admin/v4/tpl/gq_share/v1/share_layer_list.html?style=rank_tags&title=%E6%8C%96%E8%A7%86%E9%A2%91%E7%83%AD%E9%97%A8%E6%8E%92%E8%A1%8C%E6%A6%9C&width=276&height=260&border=false&usrId=r239612568"></iframe>
     </div>
     
     <!-- 详情页侧边栏第1个广告位开始 -->
