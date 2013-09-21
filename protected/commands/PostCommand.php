092285b868fd2f1cfd181e2c72de875bda9904e1
Dong Chen <cdcchen@gmail.com>
2013-09-22 00:32:08 +0800

    把视频更新单独做成一个action，保持一次更新一个视频

--- protected/commands/PostCommand.php
+++ protected/commands/PostCommand.php
@@ -19,24 +19,45 @@ class PostCommand extends CConsoleCommand
         $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_IMAGE);
         $lengtuIDs = $cmd->where($conditions, $params)->queryColumn();
         
-        // 视频
-        $cmd->limit(1);
-        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_VIDEO);
-        $videoIDs = $cmd->where($conditions, $params)->queryColumn();
-        
         // 挖热点
-        $cmd->limit($count);
         $conditions = array('and', 'channel_id = :channelID', 'state = :disable_state');
         $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FOCUS);
         $focusIDs = $cmd->where($conditions, $params)->queryColumn();
         
-        $ids = array_merge($duanziIDs, $lengtuIDs, $videoIDs, $focusIDs);
+        $ids = array_merge($duanziIDs, $lengtuIDs, $focusIDs);
         $ids = array_unique($ids);
         
         $nums = 0;
         foreach ($ids as $index => $id) {
             $num = app()->getDb()->createCommand()
-                ->update('{{post}}',
+                ->update(TABLE_POST,
+                    array('state'=>POST_STATE_ENABLED, 'create_time'=>time() - $index*60),
+                    'id = :pid',
+                    array(':pid' => $id)
+                );
+            
+            if ($num > 0) $nums++;
+        }
+        printf("update %d rows\n", $nums);
+    }
+    
+    public function actionUpdateVideoStateEnable($count)
+    {
+        $cmd = app()->getDb()->createCommand()
+            ->select('id')
+            ->from(TABLE_POST)
+            ->order('create_time desc, id desc')
+            ->limit($count);
+        
+        // 视频
+        $conditions = array('and', 'channel_id = :channelID', 'media_type = :mediatype', 'state = :disable_state');
+        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_VIDEO);
+        $videoIDs = $cmd->where($conditions, $params)->queryColumn();
+        
+        $nums = 0;
+        foreach ($videoIDs as $index => $id) {
+            $num = app()->getDb()->createCommand()
+                ->update(TABLE_POST,
                     array('state'=>POST_STATE_ENABLED, 'create_time'=>time() - $index*60),
                     'id = :pid',
                     array(':pid' => $id)
