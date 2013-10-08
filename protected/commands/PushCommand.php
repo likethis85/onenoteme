b837cc821261ac56daffdf7c4d735578438b65e2
Dong Chen <cdcchen@gmail.com>
2013-10-08 13:00:32 +0800

    thrthrth

--- protected/commands/PushCommand.php
+++ protected/commands/PushCommand.php
@@ -11,7 +11,7 @@ class PushCommand extends CConsoleCommand
     public function actionTest()
     {
         $channel = new Channel(BAIDU_APP_WDZ_APP_KEY, BAIDU_APP_WDZ_SECRET_KEY);
-//         $channel->setHost(Channel::HOST_IOS_DEV);
+        $channel->setHost(Channel::HOST_IOS_DEV);
         $optional[Channel::USER_ID] = '1032337158820187361';
         $optional[Channel::DEVICE_TYPE] = BAIDU_DEVICE_TYPE_IOS;
         $optional[Channel::MESSAGE_TYPE] = BAIDU_MESSAGE_TYPE_ALERT;
