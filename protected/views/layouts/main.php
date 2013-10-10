7c2aaee65001f092f3bfefdd6e8f5b60ee7d8457
Dong Chen <cdcchen@gmail.com>
2013-10-10 16:35:57 +0800

    wegegeg

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -64,6 +64,9 @@ _hmt && _hmt.push(['_setCustomVar', 1, 'guest', <?php echo (int)user()->isGuest;
     <?php $this->widget('CDAdvert', array('solt'=>'top_banner', 'boxClass'=>'bottom15px'));?>
 	<?php echo $content;?>
 </div>
+
+<?php $this->widget('CDAdvert', array('solt'=>'bottom_banner', 'boxClass'=>'top15px'));?>
+
 <?php echo param('footer_before_html');?>
 <?php $this->renderPartial('/public/footer');?>
 
