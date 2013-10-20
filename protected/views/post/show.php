14d31d34571ddd694a364cc40c2dac1d4bde523d
Chen Dong <cdcchen@gmail.com>
2013-10-20 11:21:31 +0800

    rnrtnrtn

--- protected/views/post/show.php
+++ protected/views/post/show.php
@@ -27,12 +27,7 @@ _hmt && _hmt.push(['_setCustomVar', 2, 'channel_id', <?php echo (int)$this->chan
         <div class="post-video"><?php echo $post->video->getDesktopVideoHTML(600, 370);?></div>
         <?php endif;?>
 		
-        <?php
-        	if ($post->getIsTextType())
-        		$this->widget('CDAdvert', array('solt'=>'post_content_bottom_01'));
-        	else
-        		$this->widget('CDAdvert', array('solt'=>'post_content_bottom_02'));
-        ?>
+        <?php $this->widget('CDAdvert', array('solt'=>'post_content_bottom_01'));?>
         
         <?php if ($prevUrl || $nextUrl):?>
         <div class="change-post clearfix">
@@ -45,6 +40,8 @@ _hmt && _hmt.push(['_setCustomVar', 2, 'channel_id', <?php echo (int)$this->chan
             <div class="clear"></div>
         </div>
         <?php endif;?>
+        
+        <?php $this->widget('CDAdvert', array('solt'=>'post_content_bottom_02'));?>
         
         <div class="item-toolbar">
             <ul>
