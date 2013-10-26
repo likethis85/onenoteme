60da07e0dee4d12852cc0466f50b2eb902d82ec3
Chen Dong <cdcchen@gmail.com>
2013-10-26 23:11:44 +0800

    erberbreb

--- protected/commands/PostCommand.php
+++ protected/commands/PostCommand.php
@@ -9,27 +9,23 @@ class PostCommand extends CConsoleCommand
             ->order('create_time desc, id desc')
             ->limit($count);
         
-        $jokeConditions = array('and', 'channel_id = :channelID', 'media_type = :mediatype', 'state = :disable_state');
+        $conditions = array('and', 'channel_id = :channelID', 'media_type = :mediatype', 'state = :disable_state');
         
         // 文字
-        $textParams = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_TEXT);
-        $duanziIDs = $cmd->where($jokeConditions, $textParams)->queryColumn();
-        var_dump($cmd->text);
+        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_TEXT);
+        $duanziIDs = $cmd->where($conditions, $params)->queryColumn();
+        
         // 图片
-        $lengtuParams = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_IMAGE);
-        $lengtuIDs = $cmd->where($jokeConditions, $lengtuParams)->queryColumn();
-        var_dump($cmd->text);
+        $cmd->where = $cmd->params = null;
+        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_IMAGE);
+        $lengtuIDs = $cmd->where($conditions, $params)->queryColumn();
         
         // 挖热点
-        $cmd = app()->getDb()->createCommand()
-            ->select('id')
-            ->from(TABLE_POST)
-            ->order('create_time desc, id desc')
-            ->limit($count);
-        $focusConditions = array('and', 'channel_id = :channelID', 'state = :disable_state');
-        $foucsParams = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FOCUS);
-        $focusIDs = $cmd->where($focusConditions, $foucsParams)->queryColumn();
-        var_dump($cmd->text);
+        $cmd->where = $cmd->params = null;
+        $conditions = array('and', 'channel_id = :channelID', 'state = :disable_state');
+        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FOCUS);
+        $focusIDs = $cmd->where($conditions, $params)->queryColumn();
+        
         $ids = array_merge($duanziIDs, $lengtuIDs, $focusIDs);
         $ids = array_unique($ids);
         
