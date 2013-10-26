fbf0c5c50b549d7848a85eaa49d909875ca978cb
Chen Dong <cdcchen@gmail.com>
2013-10-26 23:03:10 +0800

    rbebrb

--- protected/commands/PostCommand.php
+++ protected/commands/PostCommand.php
@@ -9,20 +9,20 @@ class PostCommand extends CConsoleCommand
             ->order('create_time desc, id desc')
             ->limit($count);
         
-        $conditions = array('and', 'channel_id = :channelID', 'media_type = :mediatype', 'state = :disable_state');
+        $jokeConditions = array('and', 'channel_id = :channelID', 'media_type = :mediatype', 'state = :disable_state');
         
         // 文字
-        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_TEXT);
-        $duanziIDs = $cmd->where($conditions, $params)->queryColumn();
+        $textParams = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_TEXT);
+        $duanziIDs = $cmd->where($jokeConditions, $textParams)->queryColumn();
         var_dump($cmd->text);
         // 图片
-        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_IMAGE);
-        $lengtuIDs = $cmd->where($conditions, $params)->queryColumn();
+        $lengtuParams = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FUNNY, ':mediatype'=>MEDIA_TYPE_IMAGE);
+        $lengtuIDs = $cmd->where($jokeConditions, $lengtuParams)->queryColumn();
         var_dump($cmd->text);
         // 挖热点
-        $conditions = array('and', 'channel_id = :channelID', 'state = :disable_state');
-        $params = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FOCUS);
-        $focusIDs = $cmd->where($conditions, $params)->queryColumn();
+        $focusConditions = array('and', 'channel_id = :channelID', 'state = :disable_state');
+        $foucsParams = array(':disable_state'=>POST_STATE_DISABLED, ':channelID'=>CHANNEL_FOCUS);
+        $focusIDs = $cmd->where($focusConditions, $lengtuParams)->queryColumn();
         var_dump($cmd->text);
         $ids = array_merge($duanziIDs, $lengtuIDs, $focusIDs);
         var_dump($ids);
