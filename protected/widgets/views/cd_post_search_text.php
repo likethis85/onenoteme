e47e0bf5b5c04b88515be6c671717f406f07ae9e
Chen Dong <cdcchen@gmail.com>
2013-10-27 12:28:58 +0800

    erbrbrtb

--- protected/widgets/views/cd_post_search_text.php
+++ protected/widgets/views/cd_post_search_text.php
@@ -1,6 +1,6 @@
 <ul class="clearfix best-text">
     <?php foreach ($models as $model):?>
-    <li><?php echo $model->getTitleLink(30 $this->linkTarget, $this->trace);?></li>
+    <li><?php echo $model->getTitleLink(30, $this->linkTarget, $this->trace);?></li>
     <?php endforeach;?>
     <div class="clear"></div>
 </ul>
