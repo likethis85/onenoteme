4caea67cfbff4ae85c60046f024ab2a85fadd9fc
Chen Dong <cdcchen@gmail.com>
2013-11-01 23:04:47 +0800

    erberb

--- protected/commands/PushCommand.php
+++ protected/commands/PushCommand.php
@@ -13,17 +13,18 @@ class PushCommand extends CConsoleCommand
         $channel = new Channel(BAIDU_APP_WDZ_APP_KEY, BAIDU_APP_WDZ_SECRET_KEY);
         $channel->setHost(Channel::HOST_IOS_DEV);
 //         $optional[Channel::USER_ID] = '924028076706842347';
+        $optional[Channel::TAG_NAME] = 'has_logined';
         $optional[Channel::DEVICE_TYPE] = BAIDU_DEVICE_TYPE_IOS;
         $optional[Channel::MESSAGE_TYPE] = BAIDU_MESSAGE_TYPE_ALERT;
         $message = array(
             'aps' => array(
-                'alert' => 'Message From Baidu Push',
+                'alert' => 'Message From Baidu Push tag',
                 'Sound' => '',
                 'Badge' => 0
             ),
         );
         
-        $ret = $channel->pushMessage (Channel::PUSH_TO_ALL, $message, 'test_key', $optional);
+        $ret = $channel->pushMessage (Channel::PUSH_TO_TAG, $message, 'test_key', $optional);
         
         if (false === $ret) {
             echo 'WRONG, ' . __FUNCTION__ . ' ERROR!!!!!';
