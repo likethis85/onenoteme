30bce6a1418ad03b1aeb02e577a9b0d2f6cbb51a
Chen Dong <cdcchen@gmail.com>
2013-12-06 22:17:55 +0800

    wevwevwev

--- protected/config/define.php
+++ protected/config/define.php
@@ -1,9 +1,15 @@
 <?php
 
-define('MYSQL_HOST', 'waduanzi.mysql.rds.aliyuncs.com');
+//define('MYSQL_HOST', 'waduanzi.mysql.rds.aliyuncs.com');
+//define('MYSQL_PORT', '3306');
+//define('MYSQL_DBNAME', 'cd_waduanzi');
+//define('MYSQL_USER', 'waduanzi');
+//define('MYSQL_PASSWORD', 'cd_790406');
+
+define('MYSQL_HOST', 'localhost');
 define('MYSQL_PORT', '3306');
 define('MYSQL_DBNAME', 'cd_waduanzi');
-define('MYSQL_USER', 'waduanzi');
+define('MYSQL_USER', 'root');
 define('MYSQL_PASSWORD', 'cd_790406');
 
 define('CD_YES', 1);
