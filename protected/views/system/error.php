4df8218d51b85e124fc8a317e259a45947a93bf4
Chen Dong <cdcchen@gmail.com>
2013-12-24 16:08:55 +0800

    update weibo url

--- protected/views/system/error.php
+++ protected/views/system/error.php
@@ -3,8 +3,8 @@
 <p><span>不想等？可以去：</span></p>
 <div class="divider"></div>
 <ul id="error-suggestions">
-    <li><a href="http://t.sina.com.cn/ifanr" target="_blank">新浪微博</a></li>
-    <li><a href="http://t.qq.com/cdcchen" target="_blank">腾讯微博</a></li>
+    <li><a href="http://weibo.com/cdcchen" target="_blank">新浪微博</a></li>
+    <li><a href="http://t.qq.com/waduanzi" target="_blank">腾讯微博</a></li>
     <li><a href="<?php echo IPHONE_APP_URL;?>" target="_blank">下载iPhone应用</a></li>
     <li><a href="<?php echo ANDROID_APP_URL;?>" target="_blank">下载Android应用</a></li>
 </ul>
