f4c2aaf10ac26b4f211204f2a6a527f5e564ba07
Chen Dong <cdcchen@gmail.com>
2014-03-02 12:05:12 +0800

    wvwevwev

--- protected/modules/admin/controllers/WbpostController.php
+++ protected/modules/admin/controllers/WbpostController.php
@@ -58,7 +58,7 @@ class WbpostController extends AdminController
                     $opts['padding_bottom'] = (int)$_POST['padding_bottom'];
                     $opts['water_position'] = (int)$_POST['water_position'];
                     $referrer = 'http://weibo.com';
-                    if ($result = $post->fetchRemoteImagesBeforeSave($referrer, $opts)) {
+                    if (1){//$result = $post->fetchRemoteImagesBeforeSave($referrer, $opts)) {
                         $post->weibo_pic = $temp->bmiddle_pic;
                         $result = $post->save();
                     }
