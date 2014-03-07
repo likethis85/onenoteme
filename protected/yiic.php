066cfe6cb13e442c8f1d1e22a4d044df66fe76a6
Chen Dong <cdcchen@gmail.com>
2014-03-07 17:05:01 +0800

    234234

--- protected/yiic.php
+++ protected/yiic.php
@@ -4,15 +4,16 @@ YII_DEBUG || error_reporting(0);
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 defined('YII_PRODUCT') or define('YII_PRODUCT', true);
 defined('YII_DEBUG') or define('YII_DEBUG', false);
+define('CD_COMMAND_ROOT', dirname(__FILE__));
 define('CD_CONFIG_ROOT', dirname(__FILE__) . '/config');
 
 $short = dirname(__FILE__) . '/../library/shortcut.php';
-$define = CD_CONFIG_ROOT . '/config/define.php';
+$define = CD_CONFIG_ROOT . DS . 'define.php';
 require($define);
 require($short);
 
 // change the following paths if necessary
-$yiic = CD_CONFIG_ROOT . '/../library/framework/yiic.php';
-$config = CD_CONFIG_ROOT . '/config/console.php';
+$yiic = CD_COMMAND_ROOT . '/../library/framework/yiic.php';
+$config = CD_CONFIG_ROOT . DS . 'console.php';
 
 require($yiic);
--- protected/yiic_develop.php
+++ protected/yiic_develop.php
@@ -2,8 +2,8 @@
 YII_DEBUG || error_reporting(0);
 
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
-defined('YII_PRODUCT') or define('YII_PRODUCT', true);
-defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_PRODUCT') or define('YII_PRODUCT', false);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 define('CD_COMMAND_ROOT', dirname(__FILE__));
 define('CD_CONFIG_ROOT', dirname(__FILE__) . '/config');
 
